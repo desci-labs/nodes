ee296ebaa80a1394c4998bfd846393975fb7346e
Sina Iman <sina.iman@gmail.com>
2023-03-22 18:31:25 +0100

    tracing fix

--- desci-server/src/index.ts
+++ desci-server/src/index.ts
@@ -25,7 +25,7 @@ export const app = express();
 const ENABLE_TELEMETRY = process.env.NODE_ENV != 'dev';
 if (ENABLE_TELEMETRY) {
   console.log('[DeSci Nodes] Telemetry enabled');
-
+  require('./tracing');
   Sentry.init({
     dsn: 'https://d508a5c408f34b919ccd94aac093e076@o1330109.ingest.sentry.io/6619754',
     release: 'desci-nodes-server@' + process.env.npm_package_version,
