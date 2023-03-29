1fc08124718afbb99ad7ddda44d94374e5df5586
Sina Iman <sina.iman@gmail.com>
2023-03-29 22:17:55 +0200

    clean up comment

--- desci-server/src/utils/discordUtils.ts
+++ desci-server/src/utils/discordUtils.ts
@@ -6,13 +6,5 @@ const discordNotification = new DiscordNotification(
 );
 export const discordNotify = async (message: string) => {
   console.log('DISCORD NOTIFY', message, discordNotification.name, discordNotification.webhook);
-  await discordNotification
-    .sucessfulMessage()
-    .addTitle('Node Updated')
-    .addDescription(message)
-    // .addField({ name: 'Field 1', value: 'Content #1', inline: false }) //breakline
-    // .addField({ name: 'Field 2', value: 'Content #2' })
-    // .addField({ name: 'Field 3', value: 'Content #3' })
-    // .addFooter('My footer') // Small text at the end of discord notification
-    .sendMessage();
+  await discordNotification.sucessfulMessage().addTitle('Node Updated').addDescription(message).sendMessage();
 };
