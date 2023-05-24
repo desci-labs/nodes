e368325cd07a6dae1c3b9fed7e428cfcd64f61ba
kadami <kadamidev@users.noreply.github.com>
2023-05-25 02:17:14 +0700

    prepare data ref util added for preparing non pinned manifest refs

--- desci-server/src/utils/dataRefTools.ts
+++ desci-server/src/utils/dataRefTools.ts
@@ -75,6 +75,63 @@ export async function generateDataReferences(
   return [dataRootEntry, ...dataTreeToPubRef];
 }
 
+// used to prepare data refs for a given dag and manifest (differs from generateDataReferences in that you don't need the updated manifestCid ahead of time)
+export async function prepareDataRefs(
+  nodeUuid: string,
+  manifest: ResearchObjectV1,
+  rootDagCid: string,
+  markExternals = false,
+): Promise<Prisma.DataReferenceCreateManyInput[] | Prisma.PublicDataReferenceCreateManyInput[]> {
+  nodeUuid = nodeUuid.endsWith('.') ? nodeUuid : nodeUuid + '.';
+  const node = await prisma.node.findFirst({
+    where: {
+      uuid: nodeUuid,
+    },
+  });
+  if (!node) throw new Error(`Node not found for uuid ${nodeUuid}`);
+  const manifestEntry: ResearchObjectV1 = manifest;
+  const dataBucketCid = rootDagCid;
+
+  const dataRootEntry: Prisma.DataReferenceCreateManyInput = {
+    cid: dataBucketCid,
+    path: dataBucketCid,
+    userId: node.ownerId,
+    root: true,
+    rootCid: dataBucketCid,
+    directory: true,
+    size: 0,
+    type: DataType.DATA_BUCKET,
+    nodeId: node.id,
+    ...(markExternals ? { external: null } : {}),
+  };
+
+  const externalCidMap = await generateExternalCidMap(node.uuid);
+  let dataTree = recursiveFlattenTree(await getDirectoryTree(dataBucketCid, externalCidMap));
+  if (markExternals) {
+    dataTree = recursiveFlattenTree(await discoveryLs(dataBucketCid, externalCidMap));
+  }
+  const manifestPathsToDbTypes = generateManifestPathsToDbTypeMap(manifestEntry);
+
+  const dataTreeToPubRef: Prisma.DataReferenceCreateManyInput[] = dataTree.map((entry) => {
+    const neutralPath = neutralizePath(entry.path);
+    const dbType = inheritComponentType(neutralPath, manifestPathsToDbTypes);
+    return {
+      cid: entry.cid,
+      path: entry.path,
+      userId: node.ownerId,
+      rootCid: dataBucketCid,
+      root: false,
+      directory: entry.type === 'dir',
+      size: entry.size,
+      type: dbType,
+      nodeId: node.id,
+      ...(!markExternals ? {} : entry.external ? { external: true } : { external: null }),
+    };
+  });
+
+  return [dataRootEntry, ...dataTreeToPubRef];
+}
+
 interface DataReferenceDiff {
   currentRef: Partial<DataReference>;
   requiredRef: Partial<DataReference>;
