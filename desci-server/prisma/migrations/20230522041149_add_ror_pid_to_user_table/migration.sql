863327181fff7d3b668f27f8e2498ee7818be55a
Sina Iman <sina.iman@gmail.com>
2023-05-26 17:22:10 +0200

    consolidate migrations

--- desci-server/prisma/migrations/20230522041149_add_ror_pid_to_user_table/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `rorpid` on the `User` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "User" DROP COLUMN "rorpid",
-ADD COLUMN     "rorPid" TEXT[] DEFAULT ARRAY[]::TEXT[];
