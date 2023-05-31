84e976e5bc168b75fcf8a2c2e4a0068d6a05cddd
kadami <kadamidev@users.noreply.github.com>
2023-06-01 03:20:23 +0700

    added redis client init, and helpers

---
+++ desci-server/src/redis.ts
@@ -0,0 +1,28 @@
+import redis from 'redis';
+
+const redisClient = redis.createClient({
+  url: process.env.REDIS_URL,
+});
+
+redisClient.on('error', (err) => console.error('Redis error:', err));
+
+// gracefully shutdown
+process.on('exit', () => {
+  redisClient.quit();
+});
+
+export default redisClient;
+
+const DEFAULT_TTL = 60 * 60 * 24 * 7; // 1 week
+
+export function getOrCache<T>(key: string, fn: () => Promise<T>, ttl = DEFAULT_TTL) {
+  return new Promise<T>((resolve, reject) => {
+    redisClient.get(key, async (err, result) => {
+      if (err) return reject(err);
+      if (result !== null) return resolve(JSON.parse(result));
+      const value = await fn();
+      redisClient.setex(key, ttl, JSON.stringify(value));
+      resolve(value);
+    });
+  });
+}
