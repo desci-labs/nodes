799c87e95e119fd91685c72abc9e3ddeb0d47ae1
kadami <kadamidev@users.noreply.github.com>
2023-06-06 21:08:31 +0700

    log level configurable in env, add defaults for dev, test environments

--- desci-server/src/logger.ts
+++ desci-server/src/logger.ts
@@ -1,11 +1,13 @@
 import pino from 'pino';
 
+const logLevel = process.env.PINO_LOG_LEVEL || 'trace';
+
 const logger = pino({
   transport: {
     target: 'pino-pretty',
     options: {
       colorize: true,
-      level: 'trace',
+      level: logLevel,
     },
   },
 });
