e2d332b710d99e7db18ab5b40995fa9401664eb6
m0ar <edvard@hubinette.me>
2023-07-03 17:56:09 +0200

    Start pg if necessary in resetTables.sh

--- resetTables.sh
+++ resetTables.sh
@@ -7,24 +7,41 @@ set -euo pipefail
 
 # Load psql auth vars to env
 source <(grep -e "POSTGRES_USER" -e "POSTGRES_PASSWORD" .env)
+#DATABASE_URL="postgresql://$PG_AUTH@localhost:$PG_PORT/postgres"
+started_by_script=0
+if ! container=$(docker ps | grep db_boilerplate | cut -d' ' -f1); then
+  started_by_script=1
+  echo "Postgres container not alive, starting..."
+  container=$(docker compose \
+    --file docker-compose.yml \
+    --file docker-compose.dev.yml \
+    --compatibility \
+    run \
+    --no-deps \
+    --detach \
+    db_postgres)
 
-DATABASE_URL="postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@localhost:5433/postgres"
+  sleep 3
+fi
+
 
 drop_schema() {
   local SCHEMA=$1
   echo "[resetTables] Dropping and recreating $SCHEMA..."
-  psql -c "drop schema if exists $SCHEMA cascade" $DATABASE_URL
+  docker exec $container bash -c \
+    "psql -U $POSTGRES_USER -d postgres -c 'drop schema if exists $SCHEMA cascade'"
+  sleep 1
 }
-
-if ! docker ps | grep -q db_boilerplate; then
-  echo "[resetTables] WARN: postgres container isn't running, skipping!"
-  echo "[resetTables] re-run with cluster up to clean chain and subgraph tables"
-  exit
-fi
 
 drop_schema "chain1"
 drop_schema "subgraphs"
 drop_schema "sgd1"
+
+if [ "$started_by_script" -gt 0 ]; then
+  sleep 1
+  echo "Killing postgres container..."
+  docker stop "$container" > /dev/null
+fi
 
 echo "[resetTables] table cleanup done"
 
