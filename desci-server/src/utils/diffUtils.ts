a377d9d8b3f1bec9a9a25f25990320bf52134755
kadami <kadamidev@users.noreply.github.com>
2023-08-04 03:20:37 +0700

    added pruning functionality to diffTrees util to prevent returning gigatrees

--- desci-server/src/utils/diffUtils.ts
+++ desci-server/src/utils/diffUtils.ts
@@ -1,41 +1,69 @@
-import { DriveObject, DrivePath, neutralizePath } from '@desci-labs/desci-models';
+import { DriveObject, DrivePath, FileType, neutralizePath } from '@desci-labs/desci-models';
 
+export type TreeDiffObject = {
+  entries: DrivePath[];
+  count: number;
+};
 export interface TreeDiff {
-  added: DrivePath[];
-  removed: DrivePath[];
-  modified: DrivePath[];
+  added: TreeDiffObject;
+  removed: TreeDiffObject;
+  modified: TreeDiffObject;
 }
 
-export function diffTrees(treeA: DriveObject[], treeB: DriveObject[]): TreeDiff {
-  const diff: TreeDiff = {
-    added: [],
-    removed: [],
-    modified: [],
+interface DiffTreeOptions {
+  pruneThreshold?: number;
+  onThresholdExceeded?: {
+    onlyDirectories?: boolean;
+  };
+}
+
+export function diffTrees(treeA: DriveObject[], treeB: DriveObject[], options: DiffTreeOptions = {}) {
+  const diff = {
+    added: { entries: [], count: 0 },
+    removed: { entries: [], count: 0 },
+    modified: { entries: [], count: 0 },
   };
 
   const treeAMap = treeA.reduce((acc, node) => {
-    acc[neutralizePath(node.path)] = node.cid;
+    acc[neutralizePath(node.path)] = node;
     return acc;
   }, {});
 
   const treeBMap = treeB.reduce((acc, node) => {
-    acc[neutralizePath(node.path)] = node.cid;
+    acc[neutralizePath(node.path)] = node;
     return acc;
   }, {});
 
+  const prune = (category) => {
+    if (
+      options.pruneThreshold &&
+      diff[category].count > options.pruneThreshold &&
+      options.onThresholdExceeded?.onlyDirectories
+    ) {
+      diff[category].entries = diff[category].entries.filter((path) => treeAMap[path]?.type === FileType.DIR);
+    }
+  };
+
   Object.keys(treeAMap).forEach((path) => {
     if (!treeBMap[path]) {
-      diff.added.push(path);
-    } else if (treeAMap[path] !== treeBMap[path]) {
-      diff.modified.push(path);
+      diff.added.entries.push(path);
+      diff.added.count++;
+    } else if (treeAMap[path].cid !== treeBMap[path].cid) {
+      diff.modified.entries.push(path);
+      diff.modified.count++;
     }
   });
 
   Object.keys(treeBMap).forEach((path) => {
     if (!treeAMap[path]) {
-      diff.removed.push(path);
+      diff.removed.entries.push(path);
+      diff.removed.count++;
     }
   });
+
+  prune('added');
+  prune('removed');
+  prune('modified');
 
   return diff;
 }
