9f3c967421d255486bf14194050a41f1c4e876f8
kadami <kadamidev@users.noreply.github.com>
2023-09-04 23:22:38 +0700

    add auth cookie for ssr compatability

--- desci-server/src/controllers/auth/magic.ts
+++ desci-server/src/controllers/auth/magic.ts
@@ -11,7 +11,12 @@ const generateAccessToken = (payload) => {
 };
 const oneYear = 1000 * 60 * 60 * 24 * 365;
 export const magic = async (req: Request, res: Response, next: NextFunction) => {
-  logger.info({ fn: 'magic', reqBody: req.body }, `magic link ${req.body}`);
+  if (process.env.NODE_ENV === 'production') {
+    logger.info({ fn: 'magic', email: req.body.email }, `magic link requested`);
+  } else {
+    logger.info({ fn: 'magic', reqBody: req.body }, `magic link ${req.body}`);
+  }
+
   const { email, code } = req.body;
   if (!code) {
     try {
@@ -24,6 +29,15 @@ export const magic = async (req: Request, res: Response, next: NextFunction) => 
     try {
       const user = await magicLinkRedeem(email, code);
       const token = generateAccessToken({ email: user.email });
+
+      res.cookie('auth', token, {
+        maxAge: oneYear,
+        httpOnly: true, // Ineffective whilst we still return the bearer token to the client in the response
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'lax',
+      });
+
+      // Bearer token still returned for backwards compatability, should look to remove in the future.
       res.send({ ok: true, user: { email: user.email, token } });
       saveInteraction(req, ActionType.USER_LOGIN, { userId: user.id }, user.id);
     } catch (err) {
