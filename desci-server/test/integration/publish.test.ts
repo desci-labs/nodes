1df03c5713a3e937043cf08e372892e548cdf5bb
Sina Iman <sina.iman@gmail.com>
2023-10-07 21:23:23 +0200

    checkpoint publish test

--- desci-server/test/integration/publish.test.ts
+++ desci-server/test/integration/publish.test.ts
@@ -1,18 +1,32 @@
 import 'mocha';
 import fs from 'fs';
 
-import { ResearchObjectV1, recursiveFlattenTree } from '@desci-labs/desci-models';
+import {
+  DataBucketComponent,
+  ResearchObjectComponentType,
+  ResearchObjectV1,
+  recursiveFlattenTree,
+} from '@desci-labs/desci-models';
 import { User, Node } from '@prisma/client';
 import { expect } from 'chai';
 import supertest from 'supertest';
 
 import { app } from '../../src';
 import prisma from '../../src/client';
+import { UpdateResponse } from '../../src/controllers/data';
+import { NodesDraftCreateResponse, NodesDraftCreateRequest } from '../../src/controllers/nodes/draftCreate';
+import { NodesDraftUpdateRequest } from '../../src/controllers/nodes/draftUpdate';
 import { NodesPublishRequest } from '../../src/controllers/nodes/publish';
 import { generateJwtForUser } from '../../src/services/auth';
-import { spawnEmptyManifest } from '../../src/services/ipfs';
+import {
+  addFilesToDag,
+  spawnEmptyManifest,
+  client as ipfs,
+  updateManifestAndAddToIpfs,
+  resolveIpfsDataAsResearchObject,
+} from '../../src/services/ipfs';
 import { validateDataReferences } from '../../src/utils/dataRefTools';
-import { createTestNode } from '../util';
+import { createTestNode, spawnExampleDirDag } from '../util';
 describe('Publish', () => {
   let admin: User;
   let adminToken: string | undefined;
@@ -102,7 +116,6 @@ describe('Publish', () => {
     });
   });
   it('succeeds with basic case', async () => {
-    debugger;
     const manifest = await spawnEmptyManifest();
     const { cid, node, uuid } = await createTestNode(admin, manifest);
 
@@ -128,10 +141,107 @@ describe('Publish', () => {
     expect(unusedRefs.length).to.eq(0);
   });
 
-  it('succeeds with data uploaded', async () => {});
+  it('succeeds with basic data uploaded', async () => {
+    const draftRequest: NodesDraftCreateRequest = {
+      title: 'Test Node',
+      links: { pdf: [], code: [] },
+    };
 
-  it('succeeds with github repo uploaded', async () => {});
-  it('succeeds with github repo uploaded', async () => {});
+    const resp = await request
+      .post('/v1/nodes/createDraft')
+      .set('authorization', `Bearer ${adminToken}`)
+      .send(draftRequest);
 
-  it('succeeds with github repo uploaded', async () => {});
+    const createResponse = resp.body as NodesDraftCreateResponse;
+
+    // upload data to node
+    const baseManifest = await resolveIpfsDataAsResearchObject(createResponse.hash);
+    const uuid = createResponse.node.uuid!;
+    const respUpload = await request
+      .post('/v1/data/update')
+      .set('authorization', `Bearer ${adminToken}`)
+      .field('uuid', uuid)
+      .field('manifest', JSON.stringify(baseManifest))
+      .field('contextPath', 'root')
+      .attach('files', Buffer.from('test'), 'test.txt');
+
+    const updateResponse = respUpload.body as UpdateResponse;
+
+    expect(respUpload.statusCode).to.equal(200);
+
+    const cid = updateResponse.manifestCid;
+    const manifest = updateResponse.manifest;
+    const nodesPublishRequest: NodesPublishRequest = {
+      uuid,
+      cid,
+      manifest: manifest,
+      transactionId: '0x0',
+    };
+    const publishResp = await request
+      .post(PUBLISH_ROUTE)
+      .set('authorization', `Bearer ${adminToken}`)
+      .send(nodesPublishRequest);
+
+    expect(publishResp.status).to.equal(200);
+    const { missingRefs, unusedRefs, diffRefs } = await validateDataReferences({
+      nodeUuid: uuid,
+      manifestCid: cid,
+      publicRefs: false,
+      markExternals: false,
+    });
+    expect(missingRefs.length).to.eq(0);
+    expect(unusedRefs.length).to.eq(0);
+  });
+
+  it('succeeds with basic data uploaded', async () => {
+    const draftRequest: NodesDraftCreateRequest = {
+      title: 'Test Node',
+      links: { pdf: [], code: [] },
+    };
+
+    const resp = await request
+      .post('/v1/nodes/createDraft')
+      .set('authorization', `Bearer ${adminToken}`)
+      .send(draftRequest);
+
+    const createResponse = resp.body as NodesDraftCreateResponse;
+
+    // upload data to node
+    const baseManifest = await resolveIpfsDataAsResearchObject(createResponse.hash);
+    const uuid = createResponse.node.uuid!;
+    const respUpload = await request
+      .post('/v1/data/update')
+      .set('authorization', `Bearer ${adminToken}`)
+      .field('uuid', uuid)
+      .field('manifest', JSON.stringify(baseManifest))
+      .field('contextPath', 'root')
+      .attach('files', Buffer.from('test'), 'test.txt');
+
+    const updateResponse = respUpload.body as UpdateResponse;
+
+    expect(respUpload.statusCode).to.equal(200);
+
+    const cid = updateResponse.manifestCid;
+    const manifest = updateResponse.manifest;
+    const nodesPublishRequest: NodesPublishRequest = {
+      uuid,
+      cid,
+      manifest: manifest,
+      transactionId: '0x0',
+    };
+    const publishResp = await request
+      .post(PUBLISH_ROUTE)
+      .set('authorization', `Bearer ${adminToken}`)
+      .send(nodesPublishRequest);
+
+    expect(publishResp.status).to.equal(200);
+    const { missingRefs, unusedRefs, diffRefs } = await validateDataReferences({
+      nodeUuid: uuid,
+      manifestCid: cid,
+      publicRefs: true,
+      markExternals: false,
+    });
+    expect(missingRefs.length).to.eq(0);
+    expect(unusedRefs.length).to.eq(0);
+  });
 });
