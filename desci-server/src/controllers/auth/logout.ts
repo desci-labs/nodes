f987ef8fb634e1226f50caa5c6e94cd86191a969
Sina Iman <sina.iman@gmail.com>
2023-11-09 01:42:14 +0100

    really sign out

--- desci-server/src/controllers/auth/logout.ts
+++ desci-server/src/controllers/auth/logout.ts
@@ -13,5 +13,25 @@ export const logout = async (req: Request, res: Response, next: NextFunction) =>
     domain: process.env.NODE_ENV === 'production' ? '.desci.com' : 'localhost',
     sameSite: 'strict',
   });
+
+  (process.env.COOKIE_DOMAIN.split(',') || [undefined]).map((domain) => {
+    res.clearCookie('auth', {
+      maxAge: oneYear,
+      httpOnly: true, // Ineffective whilst we still return the bearer token to the client in the response
+      secure: process.env.NODE_ENV === 'production',
+      domain: process.env.NODE_ENV === 'production' ? domain || '.desci.com' : 'localhost',
+      sameSite: 'strict',
+    });
+  });
+
+  if (process.env.SERVER_URL === 'https://nodes-api-dev.desci.com') {
+    // insecure cookie for local dev, should only be used for testing
+    res.clearCookie('auth', {
+      maxAge: oneYear,
+      httpOnly: true,
+      sameSite: 'strict',
+    });
+  }
+
   res.send('Logged out successfully');
 };
--- desci-server/src/utils/sendCookie.ts
+++ desci-server/src/utils/sendCookie.ts
@@ -4,12 +4,14 @@ import { oneDay, oneYear } from 'controllers/auth';
 import logger from 'logger';
 export const sendCookie = (res: Response, token: string, isDevMode: boolean) => {
   if (!isDevMode) {
-    res.cookie('auth', token, {
-      maxAge: oneYear,
-      httpOnly: true, // Ineffective whilst we still return the bearer token to the client in the response
-      secure: process.env.NODE_ENV === 'production',
-      domain: process.env.NODE_ENV === 'production' ? process.env.COOKIE_DOMAIN || '.desci.com' : 'localhost',
-      sameSite: 'strict',
+    (process.env.COOKIE_DOMAIN.split(',') || [undefined]).map((domain) => {
+      res.cookie('auth', token, {
+        maxAge: oneYear,
+        httpOnly: true, // Ineffective whilst we still return the bearer token to the client in the response
+        secure: process.env.NODE_ENV === 'production',
+        domain: process.env.NODE_ENV === 'production' ? domain || '.desci.com' : 'localhost',
+        sameSite: 'strict',
+      });
     });
   }
 
