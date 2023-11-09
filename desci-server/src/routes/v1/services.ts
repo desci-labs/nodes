8c89646aebdeb5ad125c4e04103bf93066fd04ab
Sina Iman <sina.iman@gmail.com>
2023-11-09 08:42:39 +0100

    write DID to orcid + create API for orcid-did profile

--- desci-server/src/routes/v1/services.ts
+++ desci-server/src/routes/v1/services.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 
-import { orcidProfile } from 'controllers/proxy/orcidProfile';
+import { orcidDid, orcidProfile } from 'controllers/proxy/orcidProfile';
 
 const router = Router();
 
 router.get('/orcid/profile/:orcidId', [], orcidProfile);
+router.get('/orcid/did/:did', [], orcidDid);
 
 export default router;
