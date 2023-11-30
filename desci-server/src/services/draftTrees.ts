29ac5f970c7eadcdc307a0d31665076db4a88be7
kadami <kadamidev@users.noreply.github.com>
2023-12-01 03:13:27 +0700

    add processing helpers for draftNodeTrees

--- desci-server/src/services/draftTrees.ts
+++ desci-server/src/services/draftTrees.ts
@@ -6,6 +6,7 @@ import { ipfsDagToDraftNodeTreeEntries } from 'utils/draftTreeUtils';
 import { generateExternalCidMap } from 'utils/driveUtils';
 
 import { extractRootDagCidFromManifest, getManifestFromNode } from './data/processing';
+import { createDuplicateFileError, createMixingExternalDataError } from './data/processingErrors';
 import { getDirectoryTree } from './ipfs';
 
 const logger = parentLogger.child({
@@ -31,4 +32,58 @@ export async function migrateIpfsTreeToNodeTree(nodeUuid: string) {
     skipDuplicates: true,
   });
   logger.info(`Migrated IPFS tree to DraftNodeTree for node ${nodeUuid}`);
+}
+
+export async function externalDirCheck(nodeId: number, path: string): Promise<boolean> {
+  const contextDraftTreeNode = await prisma.draftNodeTree.findFirst({
+    where: { nodeId, path },
+  });
+  if (contextDraftTreeNode?.external) throw createMixingExternalDataError();
+  return false;
+}
+
+export interface EnsureUniquePathsDraftTreeParams {
+  nodeId: number;
+  contextPath: string;
+  filesBeingAdded?: any[];
+  externalUrlFilePaths?: string[];
+}
+
+export async function ensureUniquePathsDraftTree({
+  contextPath,
+  filesBeingAdded,
+  externalUrlFilePaths,
+}: EnsureUniquePathsDraftTreeParams): Promise<boolean> {
+  // Ensure all baths being added are unique to prevent collisions
+
+  let newPathsFormatted: string[] = [];
+  const header = contextPath;
+  if (filesBeingAdded?.length) {
+    newPathsFormatted = filesBeingAdded.map((f) => {
+      if (f.originalname[0] !== '/') f.originalname = '/' + f.originalname;
+      return header + f.originalname;
+    });
+  }
+  if (externalUrlFilePaths) {
+    if (externalUrlFilePaths?.length > 0) {
+      newPathsFormatted = externalUrlFilePaths.map((filePath) => {
+        return header + '/' + filePath;
+      });
+    }
+  }
+
+  const matches = await prisma.draftNodeTree.findMany({
+    where: {
+      path: {
+        in: newPathsFormatted,
+      },
+    },
+  });
+
+  const hasDuplicates = matches.length;
+  if (hasDuplicates) {
+    logger.info('[UPDATE DATASET] Rejected as duplicate paths were found');
+    throw createDuplicateFileError();
+  }
+  return true;
 }
