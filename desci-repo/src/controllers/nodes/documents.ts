e6d778f241f81a7a7e230cf4420ce481a3e12592
shadrach-tayo <shadrachtemitayo@gmail.com>
2023-12-04 01:50:10 +0100

    chore: post setup: add envs and  share types and clean up

--- desci-repo/src/controllers/nodes/documents.ts
+++ desci-repo/src/controllers/nodes/documents.ts
@@ -1,11 +1,31 @@
 import { Request, Response } from 'express';
 import server from '../../server.js';
+import { ResearchObjectDocument } from '../../types.js';
+import { ResearchObjectComponentType, ResearchObjectV1 } from '@desci-labs/desci-models';
+
+const researchObject: ResearchObjectV1 = {
+  title: '',
+  version: 'desci-nodes-0.2.0',
+  components: [
+    {
+      id: 'root',
+      name: 'root',
+      type: ResearchObjectComponentType.DATA_BUCKET,
+      payload: {
+        cid: 'bafybeicrsddlvfbbo5s3upvjbtb5flc73iupxfy2kf3rv43kkbvegbqbwq',
+        path: 'root',
+      },
+    },
+  ],
+  authors: [],
+  researchFields: [],
+  defaultLicense: 'CC BY',
+};
 
 const getNodeDocument = async function (req: Request, res: Response) {
-  console.log('REQ', req.params, req.query);
   try {
+    console.log('REQ', req.params, req.query);
     const repo = server.repo;
-    console.log('REPO', repo.networkSubsystem.peerId);
     res.status(200).send({ documentId: repo.networkSubsystem.peerId });
   } catch (err) {
     console.log(err);
@@ -17,6 +37,20 @@ const createNodeDocument = async function (req: Request, res: Response) {
   console.log('REQ', req.params, req.query);
   try {
     const repo = server.repo;
+    console.log('REPO', repo.networkSubsystem.peerId);
+    console.log(req.body, req.params, req.query);
+    const uuid = req?.params.uuid;
+    const handle = repo.create<ResearchObjectDocument>();
+    console.log('[AUTOMERGE]::[HANDLE NEW]', { uuid }, handle.url, handle.documentId);
+    handle.change((d) => {
+      d.manifest = researchObject;
+      d.uuid = uuid;
+    });
+    this.nodeUuidToDocIcMap.set(uuid, handle.documentId);
+    handle.docSync();
+
+    const document = await handle.doc();
+    console.log('[AUTOMERGE]::[HANDLE NEW CHANGED]', handle.url, handle.isReady(), document);
     console.log('REPO', repo);
     res.status(200).send({ ok: true, documentId: '' });
   } catch (err) {
--- desci-repo/src/middleware/ensureApiKey.ts
+++ desci-repo/src/middleware/ensureApiKey.ts
@@ -1,9 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
-const MEDIA_SERVER_API_KEY = process.env.MEDIA_SECRET_KEY;
+const REPO_SERVICE_API_KEY = process.env.REPO_SERVICE_SECRET_KEY;
 
 export const ensureApiKey = async (req: Request, res: Response, next: NextFunction) => {
   const apiKey = req.headers['x-api-key'];
-  if (!apiKey || apiKey !== MEDIA_SERVER_API_KEY) {
+  if (!apiKey || apiKey !== REPO_SERVICE_API_KEY) {
     res.sendStatus(401);
     return;
   }
