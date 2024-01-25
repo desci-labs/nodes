40ed9fa39cc663016447ef5e5e2bab3c3c4ea5f5
Sina Iman <sina.iman@gmail.com>
2024-01-25 01:02:30 -0800

    create folder

--- desci-repo/scripts/be-node-test.sh
+++ desci-repo/scripts/be-node-test.sh
@@ -5,6 +5,7 @@ echo "Install bash and execute 'wait-for-it.sh' script"
 apt-get add --update bash
 ./desci-repo/scripts/wait-for-it.sh $PG_HOST:5434 --timeout=5 --strict -- echo "postgres up and running"
 
+mkdir -p /app/desci-repo/repo-tmp
 chmod -R 777 /app/desci-repo/repo-tmp
 cd desci-repo
 yarn dev
