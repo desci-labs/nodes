d0767132309d731e0d5b51d2c6fd07817b420113
kadami <kadamidev@users.noreply.github.com>
2024-02-20 04:05:03 +0700

    config/setup, add Dockerfile

---
+++ desci-media-isolated/src/config/index.ts
@@ -0,0 +1,4 @@
+export const IPFS_GATEWAY = process.env.IPFS_GATEWAY;
+export const TEMP_DIR = '.temp';
+export const THUMBNAIL_FILES_DIR = `${TEMP_DIR}/files`;
+export const THUMBNAIL_OUTPUT_DIR = `${TEMP_DIR}/thumbnails`;
