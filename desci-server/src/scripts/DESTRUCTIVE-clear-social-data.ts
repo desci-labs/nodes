41fa9f6b6977193faf0122c05d634f847507cf94
Sina Iman <sina.iman@gmail.com>
2024-02-20 23:43:49 -0800

    small social tweaks, fix clear script

--- desci-server/src/scripts/DESTRUCTIVE-clear-social-data.ts
+++ desci-server/src/scripts/DESTRUCTIVE-clear-social-data.ts
@@ -8,10 +8,11 @@ export const clearSocialData = async () => {
   });
   await prisma.communityEntryAttestation.deleteMany({});
   await prisma.attestationVersion.deleteMany({});
-  await prisma.attestation.deleteMany({});
   await prisma.nodeAttestationReaction.deleteMany({});
   await prisma.nodeAttestationVerification.deleteMany({});
   await prisma.nodeAttestation.deleteMany({});
+  await prisma.attestationVersion.deleteMany({});
+  await prisma.attestation.deleteMany({});
   await prisma.desciCommunity.deleteMany({});
 };
 
