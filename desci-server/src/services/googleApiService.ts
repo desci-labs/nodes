c33bd14a3ffd0c6b596f1357f05a417a1367e033
kadami <kadamidev@users.noreply.github.com>
2024-03-08 04:40:28 +0700

    fix auth issues, fix md call

--- desci-server/src/services/googleApiService.ts
+++ desci-server/src/services/googleApiService.ts
@@ -6,21 +6,25 @@ import { google, drive_v3 } from 'googleapis';
 import { logger as parentLogger } from '../logger.js';
 
 export class GoogleApiService {
+  private oauth2Client;
   private driveClient: drive_v3.Drive;
   private logger;
 
   constructor(accessToken: string) {
-    const oauth2Client = new google.auth.OAuth2();
-    oauth2Client.setCredentials({ access_token: accessToken });
-    this.driveClient = google.drive({ version: 'v3', auth: oauth2Client });
+    this.oauth2Client = new google.auth.OAuth2({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      // clientSecret: process.env.GOOGLE_CLIENT_SECRET, Unnecessary unless we switch to 2step server-side OAuth flow
+    });
+    this.oauth2Client.setCredentials({ access_token: accessToken });
+    this.driveClient = google.drive({ version: 'v3', auth: this.oauth2Client });
     this.logger = parentLogger.child({ module: 'Services::GoogleApiService' });
   }
 
-  async getFileMetadata(docId: string): Promise<GaxiosResponse<drive_v3.Schema$File>> {
+  async getFileMetadata(docId: string): Promise<drive_v3.Schema$File> {
     try {
-      const fileMetadata = await this.driveClient.files.get({ fileId: docId });
+      const fileMetadata = await this.driveClient.files.get({ fileId: docId, fields: 'id, name, mimeType, size' });
 
-      return fileMetadata as GaxiosResponse<drive_v3.Schema$File>;
+      return fileMetadata.data;
     } catch (error) {
       this.logger.error({ docId, error }, 'Failed to get file metadata');
       throw error;
@@ -34,6 +38,31 @@ export class GoogleApiService {
       return response.data;
     } catch (error) {
       this.logger.error({ docId, error }, 'Failed to get file stream');
+      throw error;
+    }
+  }
+
+  async authenticateWithAccessToken(accessToken: string): Promise<void> {
+    try {
+      this.oauth2Client.setCredentials({ access_token: accessToken });
+      this.logger.info('Successfully authenticated with access token');
+    } catch (error) {
+      this.logger.error({ error }, 'Failed to authenticate with access token');
+      throw error;
+    }
+  }
+
+  /**
+   * Can be used later if we switch to 2-step, server-side OAuth flow, useful if we need >60 minutes of access.
+   */
+  async exchangeCodeForToken(code: string): Promise<void> {
+    try {
+      const { tokens } = await this.oauth2Client.getToken(code);
+      debugger;
+      this.oauth2Client.setCredentials(tokens);
+      this.logger.info('Successfully exchanged code for tokens');
+    } catch (error) {
+      this.logger.error({ error }, 'Failed to exchange code for tokens');
       throw error;
     }
   }
