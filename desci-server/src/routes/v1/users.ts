f269b67527dfdf0f66975f86aa999cd45ff32adf
kadami <kadamidev@users.noreply.github.com>
2024-03-15 05:10:10 +0700

    added basic user search endpoint

--- desci-server/src/routes/v1/users.ts
+++ desci-server/src/routes/v1/users.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { list, associateWallet, updateProfile, associateOrcidWallet } from '../../controllers/users/index.js';
+import { searchProfiles } from '../../controllers/users/search.js';
 import { usage } from '../../controllers/users/usage.js';
 import { ensureAdmin } from '../../middleware/ensureAdmin.js';
 import { ensureUser } from '../../middleware/permissions.js';
@@ -12,5 +13,6 @@ router.get('/', [ensureUser, ensureAdmin], list);
 router.post('/associate', [ensureUser], associateWallet);
 router.post('/orcid/associate', [ensureUser], associateOrcidWallet);
 router.patch('/updateProfile', [ensureUser], updateProfile);
+router.get('/search', [ensureUser], searchProfiles);
 
 export default router;
