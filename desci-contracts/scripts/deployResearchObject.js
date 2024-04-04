89c36e0a6c8d94c96e66b4f6b9cf50658d476641
m0ar <edvard@hubinette.me>
2024-04-04 14:00:14 +0200

    contracts: use ResearchObjectMigrated instead of ResearchObjectV2 in local deploy

--- desci-contracts/scripts/deployResearchObject.js
+++ desci-contracts/scripts/deployResearchObject.js
@@ -8,15 +8,14 @@ async function main() {
   fs.rmSync(".openzeppelin/unknown-research-object.json", { force: true });
   fs.rmSync(".openzeppelin/unknown-1337.json", { force: true });
 
-  const ResearchObjectV2 = await ethers.getContractFactory("ResearchObjectV2");
-  console.log(`[deployResearchObject] Deploying ResearchObjectV2 with dpid proxy ${dpidProxyAddress}...`);
+  const ResearchObjectV2 = await ethers.getContractFactory("ResearchObjectMigrated");
+  console.log(`[deployResearchObject] Deploying ResearchObjectMigrated with dpid proxy ${dpidProxyAddress}...`);
   const proxy = await upgrades.deployProxy(
     ResearchObjectV2,
     [ dpidProxyAddress ],
-    { initializer: "__ResearchObjectV2_init" },
   );
   await proxy.deployed();
-  console.log("[deployResearchObject] ResearchObjectV2 deployed to:", proxy.address);
+  console.log("[deployResearchObject] ResearchObjectMigrated deployed to:", proxy.address);
 
   fs.renameSync(
     ".openzeppelin/unknown-1337.json",
