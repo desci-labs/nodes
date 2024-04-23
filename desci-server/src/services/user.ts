a71c870be69d02be518a4a7395bcaccb51d0e2a6
shadrach-tayo <shadrachtemitayo@gmail.com>
2024-04-23 23:35:13 +0200

    fix: hanging tests

--- desci-server/src/services/user.ts
+++ desci-server/src/services/user.ts
@@ -240,25 +240,37 @@ export async function setOrcidForUser(
       });
       logger.trace({ fn: 'setOrcidForUser' }, 'updated user');
       const exists = await client.authToken.findFirst({ where: { userId, source: AuthTokenSource.ORCID } });
-      await client.authToken.upsert({
-        where: {
-          id: exists.id,
-        },
-        create: {
-          accessToken: auth.accessToken,
-          refreshToken: auth.refreshToken,
-          expiresIn: auth.expiresIn,
-          userId,
-          source: AuthTokenSource.ORCID,
-        },
-        update: {
-          accessToken: auth.accessToken,
-          refreshToken: auth.refreshToken,
-          expiresIn: auth.expiresIn,
-          userId,
-          source: AuthTokenSource.ORCID,
-        },
-      });
+      if (exists) {
+        await client.authToken.upsert({
+          where: {
+            id: exists.id,
+          },
+          create: {
+            accessToken: auth.accessToken,
+            refreshToken: auth.refreshToken,
+            expiresIn: auth.expiresIn,
+            userId,
+            source: AuthTokenSource.ORCID,
+          },
+          update: {
+            accessToken: auth.accessToken,
+            refreshToken: auth.refreshToken,
+            expiresIn: auth.expiresIn,
+            userId,
+            source: AuthTokenSource.ORCID,
+          },
+        });
+      } else {
+        await client.authToken.create({
+          data: {
+            accessToken: auth.accessToken,
+            refreshToken: auth.refreshToken,
+            expiresIn: auth.expiresIn,
+            userId,
+            source: AuthTokenSource.ORCID,
+          },
+        });
+      }
       logger.trace({ fn: 'setOrcidForUser' }, 'added auth token');
     } else {
       logger.trace({ fn: 'setOrcidForUser' }, 'no user found');
