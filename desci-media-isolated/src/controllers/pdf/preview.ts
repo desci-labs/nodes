32400101ba26d4f19d401f39a35d72fe1cbd3ec3
kadami <86646883+kadamidev@users.noreply.github.com>
2024-06-03 10:46:46 +0000

    added route and endpoint for returning multiple pdf previews

---
+++ desci-media-isolated/src/controllers/pdf/preview.ts
@@ -0,0 +1,55 @@
+import type { Request, Response } from 'express';
+import { ThumbnailsService } from '../../services/thumbnails.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import { TEMP_DIR, THUMBNAIL_OUTPUT_DIR } from '../../config/index.js';
+import { BadRequestError, NotFoundError } from '../../utils/customErrors.js';
+
+export type GeneratePreviewRequestBody = {
+  cid: string;
+  pages: number[];
+};
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const BASE_TEMP_DIR = path.resolve(__dirname, '../../..', TEMP_DIR);
+
+export const generatePreview = async (
+  req: Request<any, any, GeneratePreviewRequestBody, { height: number }>,
+  res: Response,
+) => {
+  const { cid, pages } = req.body;
+  const { height = 1000 } = req.query;
+
+  if (!cid) throw new BadRequestError('Missing cid in request body');
+  if (!pages) throw new BadRequestError('Missing pages number array in request body');
+
+  try {
+    const previewStreams: fs.ReadStream[] = [];
+
+    for (const pageNum of pages) {
+      const thumbnailPath = await ThumbnailsService.generateThumbnail(cid, `${cid}_${pageNum}.png`, height);
+      const fullThumbnailPath = path.join(BASE_TEMP_DIR, THUMBNAIL_OUTPUT_DIR, thumbnailPath);
+
+      // Check if the file exists before attempting to stream it
+      await new Promise<void>((resolve, reject) => {
+        fs.access(fullThumbnailPath, fs.constants.F_OK, (err) => {
+          if (err) {
+            reject(new NotFoundError(`Thumbnail not found for file with cid: ${cid}, page: ${pageNum}`));
+          } else {
+            resolve();
+          }
+        });
+      });
+
+      const readStream = fs.createReadStream(fullThumbnailPath);
+      previewStreams.push(readStream);
+    }
+
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(previewStreams);
+  } catch (err: any) {
+    return res.status(500).json({ message: err.message });
+  }
+};
--- desci-media-isolated/src/routes/v1/pdf.ts
+++ desci-media-isolated/src/routes/v1/pdf.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { generatePdfCover } from '../../controllers/pdf/createCover.js';
+import { generatePreview } from '../../controllers/pdf/preview.js';
 
 const router = Router();
 
 router.post('/addCover', generatePdfCover);
+router.post('/previews', generatePreview);
 
 export default router;
