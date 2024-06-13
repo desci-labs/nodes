47626533005872e891f48313f55e4859932df1a7
m0ar <edvard@hubinette.me>
2024-06-13 16:01:12 +0200

    server: refactor publish flow, default to dpid alias registry unless overridden

--- desci-server/src/controllers/nodes/publish.ts
+++ desci-server/src/controllers/nodes/publish.ts
@@ -1,24 +1,27 @@
 import { ResearchObjectV1 } from '@desci-labs/desci-models';
-import { ActionType, Prisma, PublishTaskQueueStatus, User } from '@prisma/client';
+import { ActionType, Node, Prisma, PublishTaskQueue, PublishTaskQueueStatus, User } from '@prisma/client';
 import { Request, Response, NextFunction } from 'express';
 
 import { prisma } from '../../client.js';
 import { logger as parentLogger } from '../../logger.js';
 import { getManifestByCid } from '../../services/data/processing.js';
-import { fixDpid, getTargetDpidUrl } from '../../services/fixDpid.js';
+import { getTargetDpidUrl } from '../../services/fixDpid.js';
 import { saveInteraction, saveInteractionWithoutReq } from '../../services/interactionLog.js';
 import {
-  publishResearchObject,
   cacheNodeMetadata,
   getAllCidsRequiredForPublish,
   createPublicDataRefs,
-  createDataMirrorJobs,
   setCeramicStream,
+  setDpidAlias,
 } from '../../services/nodeManager.js';
+<<<<<<< HEAD
 import orcidApiService from '../../services/orcid.js';
 import { publishServices } from '../../services/PublishServices.js';
+=======
+>>>>>>> a91865b (server: refactor publish flow, default to dpid alias registry unless overridden)
 import { discordNotify } from '../../utils/discordUtils.js';
 import { ensureUuidEndsWithDot } from '../../utils.js';
+import { getOrCreateDpid, upgradeDpid } from './createDpid.js';
 
 export type PublishReqBody = {
   uuid: string;
@@ -36,7 +39,7 @@ export type PublishRequest = Request<never, never, PublishReqBody> & {
 export type PublishResBody =
   | {
       ok: boolean;
-      taskId: number;
+      taskId?: number;
     }
   | {
       error: string;
@@ -98,49 +101,271 @@ export const publish = async (req: PublishRequest, res: Response<PublishResBody>
       return res.status(400).json({ error: 'failed' });
     }
 
+    // Check if there is already an ongoing publish job in the queue
     const task = await prisma.publishTaskQueue.findFirst({
       where: { uuid: ensureUuidEndsWithDot(uuid), status: { not: PublishTaskQueueStatus.FAILED } },
     });
 
     if (task) return res.status(400).json({ error: 'Node publishing in progress' });
 
+    let publishTask: PublishTaskQueue | undefined;
+
+    // Do synchronous publish if the caller passed stream information
+    if (!process.env.FALLBACK_LEGACY_DPID) {
+      await syncPublish(
+        ceramicStream,
+        commitId,
+        node,
+        owner,
+        cid,
+        uuid,
+        manifest,
+      );
+    } else {
+      publishTask = await prisma.publishTaskQueue.create({
+        data: {
+          cid,
+          dpid: manifest.dpid?.id,
+          userId: owner.id,
+          transactionId,
+          ceramicStream,
+          commitId,
+          uuid: ensureUuidEndsWithDot(uuid),
+          status: PublishTaskQueueStatus.WAITING,
+        },
+      });
+    };
+
     saveInteraction(
       req,
       ActionType.PUBLISH_NODE,
       {
         cid,
-        dpid: manifest.dpid.id,
+        dpid: manifest.dpid?.id,
         userId: owner.id,
         transactionId,
-        ceramicStream: ceramicStream ?? '',
-        commitId: commitId ?? '',
+        ceramicStream,
+        commitId,
         uuid: ensureUuidEndsWithDot(uuid),
         status: PublishTaskQueueStatus.WAITING,
       },
       owner.id,
     );
 
-    const publishTask = await prisma.publishTaskQueue.create({
-      data: {
-        cid,
-        dpid: manifest.dpid.id,
-        userId: owner.id,
-        transactionId,
-        ceramicStream: ceramicStream ?? '',
-        commitId: commitId ?? '',
-        uuid: ensureUuidEndsWithDot(uuid),
-        status: PublishTaskQueueStatus.WAITING,
-      },
-    });
-
     return res.send({
       ok: true,
-      taskId: publishTask.id,
+      taskId: publishTask?.id,
     });
   } catch (err) {
     logger.error({ err }, '[publish::publish] node-publish-err');
     return res.status(400).send({ ok: false, error: err.message });
   }
+};
+
+/**
+ * Synchronously perform publish steps before returning.
+ *
+ * This is generally fast because the ceramic update is already done,
+ * but two cases are a bit slower:
+ * 1. first time publish (wait on backend tx)
+ * 2. dpid upgrade (wait on backend tx)
+ *
+ * Semantically, these can both be made fire-and-forget promises if we can
+ * manage without instantly having the dPID alias available in this function.
+*/
+const syncPublish = async (
+  ceramicStream: string,
+  commitId: string,
+  node: Node,
+  owner: User,
+  cid: string,
+  uuid: string,
+  manifest: ResearchObjectV1,
+): Promise<void> => {
+  const logger = parentLogger.child({
+    module: 'NODE::syncPublish',
+    uuid,
+    cid,
+    ceramicStream,
+    commitId,
+  });
+
+  const latestNodeVersion = await prisma.nodeVersion.findFirst({
+    where: {
+      id: -1,
+      nodeId: node.id,
+    },
+    orderBy: {
+      id: "desc",
+    },
+  });
+
+  // Prevent duplicating the NodeVersion entry if the latest version is the same as the one we're trying to publish, as a draft save is triggered before publishing
+  const latestNodeVersionId = latestNodeVersion?.manifestUrl === cid
+    ? latestNodeVersion.id
+    : -1;
+
+  const nodeVersion = await prisma.nodeVersion.upsert({
+    where: {
+      id: latestNodeVersionId,
+    },
+    update: {
+      commitId,
+    },
+    create: {
+      nodeId: node.id,
+      manifestUrl: cid,
+      commitId,
+    },
+  });
+
+  // first time we see a stream for this node, make sure we bind it in the db
+  if (!node.ceramicStream) {
+    logger.trace(`[publish:publish] setting streamID ${ceramicStream} on node ${uuid}`);
+    await setCeramicStream(uuid, ceramicStream);
+  } else if (node.ceramicStream !== ceramicStream) {
+    logger.warn(
+      // This is unexpected and weird, but important to know if it occurs
+      `[publish:publish] stream on record does not match passed streamID`,
+      { database: node.ceramicStream, ceramicStream },
+    );
+  };
+
+  const legacyDpid = manifest.dpid?.id ? parseInt(manifest.dpid.id) : undefined;
+  let dpidAlias: number = node.dpidAlias;
+
+  // Do dataRef and dPID registration operations concurrently
+  const promises = [];
+
+  if (!dpidAlias) {
+    // The only reason this isn't just fire-and-forget is that we want the dpid
+    // for the discord notification, which won't be available otherwise for
+    // first time publishes.
+    promises.push(
+      createOrUpgradeDpidAlias(legacyDpid, ceramicStream, uuid)
+      .then(dpid => dpidAlias = dpid)
+    );
+  };
+
+  promises.push(
+    handlePublicDataRefs({
+      nodeId: node.id,
+      userId: owner.id,
+      manifestCid: cid,
+      nodeVersionId: nodeVersion.id,
+      nodeUuid: node.uuid,
+    })
+  );
+
+  await Promise.all(promises);
+
+  // TODO: different resolver url for codex? :thinking:
+  const targetDpidUrl = getTargetDpidUrl();
+
+  discordNotify(`${targetDpidUrl}/${dpidAlias}`);
+
+  /**
+   * Save the cover art for this Node for later sharing: PDF -> JPG for this version
+   */
+  cacheNodeMetadata(node.uuid, cid);
+};
+
+/**
+ * Creates new dPID if legacyDpid is falsy, otherwise tries to upgrade
+ * the dPID by binding the stream in the alias registry for that dPID.
+*/
+const createOrUpgradeDpidAlias = async (
+  legacyDpid: number | undefined,
+  ceramicStream: string,
+  uuid: string,
+): Promise<number> => {
+  let dpidAlias: number;
+  if (legacyDpid) {
+    // Requires the REGISTRY_OWNER_PKEY to be set in env
+    dpidAlias = await upgradeDpid(legacyDpid, ceramicStream);
+  } else {
+    // Will nicely return the existing dpid if this is called multiple times
+    dpidAlias = await getOrCreateDpid(ceramicStream);
+  };
+  await setDpidAlias(uuid, dpidAlias);
+  return dpidAlias;
+};
+
+type PublishData = {
+  nodeId: number,
+  nodeUuid: string,
+  userId: number,
+  manifestCid: string,
+  nodeVersionId: number,
+};
+
+const handlePublicDataRefs = async (
+  params: PublishData,
+): Promise<void> => {
+  const {
+    nodeId,
+    nodeUuid,
+    userId,
+    manifestCid,
+    nodeVersionId,
+  } = params;
+
+  const logger = parentLogger.child({
+    module: 'NODE::handlePublicDataRefs',
+    uuid: nodeUuid,
+    cid: manifestCid,
+  });
+
+  /**
+   * Publish Step 1:
+   * Create public data refs and data mirror jobs from the CIDs in the manifest
+   */
+  let cidsRequiredForPublish: Prisma.PublicDataReferenceCreateManyInput[] = [];
+  try {
+    /***
+     * Traverse the DAG structure to find all relevant CIDs and get relevant info for indexing
+     */
+    cidsRequiredForPublish = await getAllCidsRequiredForPublish(
+      manifestCid,
+      nodeUuid,
+      userId,
+      nodeId,
+      nodeVersionId
+    );
+
+    /**
+     * Index the DAGs from IPFS in order to avoid recurrent IPFS calls when requesting data in the future
+     */
+    const newPublicDataRefs = await createPublicDataRefs(
+      cidsRequiredForPublish,
+      userId,
+      nodeVersionId,
+    );
+
+    /**
+     * Save a success for configurable service quality tracking purposes
+     */
+    await saveInteractionWithoutReq(
+      ActionType.PUBLISH_NODE_CID_SUCCESS,
+      {
+        params,
+        result: { newPublicDataRefs },
+      }
+    );
+  } catch (error) {
+    logger.error({ error }, `[publish::publish] error=${error}`);
+    /**
+     * Save a failure for configurable service quality tracking purposes
+     */
+    await saveInteractionWithoutReq(
+      ActionType.PUBLISH_NODE_CID_FAIL,
+      {
+        params,
+        error
+      }
+    );
+    throw error;
+  };
 };
 
 export const publishHandler = async ({
@@ -232,107 +457,17 @@ export const publishHandler = async ({
 
     logger.trace(`[publish::publish] nodeUuid=${node.uuid}, manifestCid=${cid}, transaction=${transactionId}`);
 
-    const cidsPayload = {
-      nodeId: node.id,
-      userId: owner.id,
-      manifestCid: cid,
-      nodeVersionId: nodeVersion.id,
-      nodeUuid: node.uuid,
-    };
-
-    /**
-     * Publish Step 1:
-     * Create public data refs and data mirror jobs from the CIDs in the manifest
-     */
-    let cidsRequiredForPublish: Prisma.PublicDataReferenceCreateManyInput[] = [];
-    // debugger;
-    try {
-      /***
-       * Traverse the DAG structure to find all relevant CIDs and get relevant info for indexing
-       */
-      cidsRequiredForPublish = await getAllCidsRequiredForPublish(cid, node.uuid, owner.id, node.id, nodeVersion.id);
-
-      /**
-       * Index the DAGs from IPFS in order to avoid recurrent IPFS calls when requesting data in the future
-       */
-      const newPublicDataRefs = await createPublicDataRefs(cidsRequiredForPublish, owner.id, nodeVersion.id);
-
-      /**
-       * Create a job per mirror in order to track the status of the upload
-       * There can be multiple mirrors per node, right now there is just Estuary
-       *
-       * NOTE: uncomment when reactivating public ref mirroring
-       const dataMirrorJobs = await createDataMirrorJobs(cidsRequiredForPublish, owner.id);
-       */
-
-      // TODO: update public data refs to link versionId
-
-      /**
-       * Save a success for configurable service quality tracking purposes
-       */
-      await saveInteractionWithoutReq(ActionType.PUBLISH_NODE_CID_SUCCESS, {
-        cidsPayload,
-        result: { newPublicDataRefs },
-      });
-    } catch (error) {
-      logger.error({ error }, `[publish::publish] error=${error}`);
-      /**
-       * Save a failure for configurable service quality tracking purposes
-       */
-      await saveInteractionWithoutReq(ActionType.PUBLISH_NODE_CID_FAIL, { cidsPayload, error });
-      throw error;
-    }
-
-    /**
-     * Publish Step 2:
-     * Initiate IPFS storage upload using Estuary
-     */
-    const manifest = await getManifestByCid(cid);
-
-    const targetDpidUrl = getTargetDpidUrl();
-
-    // const researchObjectToPublish = { uuid, cid, manifest, ownerId: owner.id };
-    const sendDiscordNotification = (error: boolean) => {
-      const manifestSource = manifest as ResearchObjectV1;
-      discordNotify(`${targetDpidUrl}/${manifestSource.dpid?.id}${error ? ' (note: estuary-err)' : ''}`);
-    };
-
-    // Send an email update to all contributors
-    // await publishServices.sendVersionUpdateEmailToAllContributors({ node });
-
-    /**
-     * NOTE: uncomment when reactivating public ref mirroring
-    const handleMirrorSuccess = async (publishedResearchObjectResult) => {
-      await saveInteractionWithoutReq(ActionType.PUBLISH_NODE_RESEARCH_OBJECT_SUCCESS, {
-        researchObjectToPublish,
-        result: publishedResearchObjectResult,
-      });
-
-      sendDiscordNotification(false);
-    };
-    const handleMirrorFail = async (error) => {
-      await saveInteractionWithoutReq(ActionType.PUBLISH_NODE_RESEARCH_OBJECT_FAIL, {
-        researchObjectToPublish,
-        error,
-      });
-
-      sendDiscordNotification(true);
-    };
-
-    const publicDataReferences = await prisma.publicDataReference.findMany({
-      where: {
-        versionId: nodeVersion.id,
-      },
+    await handlePublicDataRefs({
+        nodeId: node.id,
+        nodeUuid: node.uuid,
+        userId: owner.id,
+        manifestCid: cid,
+        nodeVersionId: nodeVersion.id,
     });
-    logger.debug(
-      { publicDataReferences },
-      `[publish::publish] publicDataReferences=${JSON.stringify(publicDataReferences)}`,
-    );
 
-    // trigger ipfs storage upload, but don't wait for it to finish, will happen async
-    publishResearchObject(publicDataReferences).then(handleMirrorSuccess).catch(handleMirrorFail);
-    */
-    sendDiscordNotification(false);
+    const manifest = await getManifestByCid(cid);
+    const targetDpidUrl = getTargetDpidUrl();
+    discordNotify(`${targetDpidUrl}/${manifest.dpid?.id}`);
 
     /**
      * Save the cover art for this Node for later sharing: PDF -> JPG for this version
