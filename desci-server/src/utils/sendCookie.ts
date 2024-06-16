3bac6ee0d01b783b198fa2490ce619995616cbcc
Sina Iman <sina.iman@gmail.com>
2024-06-16 22:17:40 +0200

    make cookie auth isolated per environment, preventing collisions across nodes-api-dev nodes-api-staging and nodes-api

--- desci-server/src/utils/sendCookie.ts
+++ desci-server/src/utils/sendCookie.ts
@@ -2,12 +2,22 @@ import { type Response } from 'express';
 
 import { oneDay, oneMinute, oneYear } from '../controllers/auth/magic.js';
 import { logger } from '../logger.js';
-export const sendCookie = (res: Response, token: string, isDevMode: boolean, cookieName = 'auth') => {
+
+const AUTH_COOKIE_DOMAIN_MAPPING = {
+  'https://nodes-api.desci.com': 'auth',
+  'https://nodes-api-dev.desci.com': 'auth-dev',
+  'https://nodes-api-staging.desci.com': 'auth-stage',
+};
+
+// auth, auth-stage, auth-dev
+export const AUTH_COOKIE_FIELDNAME = AUTH_COOKIE_DOMAIN_MAPPING[process.env.SERVER_URL] || 'auth';
+
+export const sendCookie = (res: Response, token: string, isDevMode: boolean, cookieName = AUTH_COOKIE_FIELDNAME) => {
   if (isDevMode && process.env.SERVER_URL === 'https://nodes-api-dev.desci.com') {
     // insecure cookie for local dev, should only be used for testing
     logger.info({ fn: 'sendCookie' }, `insecure dev cookie set`);
     res.cookie(cookieName, token, {
-      maxAge: cookieName === 'auth' ? oneDay : oneMinute,
+      maxAge: cookieName === AUTH_COOKIE_FIELDNAME ? oneDay : oneMinute,
       httpOnly: true,
       sameSite: 'strict',
     });
@@ -16,7 +26,7 @@ export const sendCookie = (res: Response, token: string, isDevMode: boolean, coo
   (process.env.COOKIE_DOMAIN?.split(',') || [undefined]).map((domain) => {
     logger.info({ fn: 'sendCookie', domain, env: process.env.NODE_ENV }, `cookie set`);
     res.cookie(cookieName, token, {
-      maxAge: cookieName === 'auth' ? oneYear : oneMinute,
+      maxAge: cookieName === AUTH_COOKIE_FIELDNAME ? oneYear : oneMinute,
       httpOnly: true, // Ineffective whilst we still return the bearer token to the client in the response
       secure: process.env.NODE_ENV === 'production',
       domain: process.env.NODE_ENV === 'production' ? domain || '.desci.com' : 'localhost',
