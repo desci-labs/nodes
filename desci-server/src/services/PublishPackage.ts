0fd35bd4108f7ea3c8e7ff176997a54a6931a6a6
kadami <86646883+kadamidev@users.noreply.github.com>
2024-06-19 15:29:59 +0000

    enable demo prepub generations

--- desci-server/src/services/PublishPackage.ts
+++ desci-server/src/services/PublishPackage.ts
@@ -47,19 +47,23 @@ class PublishPackageService {
       this.logger.error('process.env.ISOLATED_MEDIA_SERVER_URL is not defined');
       return null;
     }
+    // debugger;
     const title = manifest.title;
-    const dpid = manifest.dpid.id;
+    const demoMode = manifest?.dpid?.id === undefined;
+    const dpid = !demoMode ? manifest?.dpid?.id : 'UNPUBLISHED_DEMO';
     if (dpid === undefined) {
       this.logger.warn({ dpid, nodeId: node.id }, 'Failed generating a publish package for node, dpid is undefined');
       throw new Error('DPID is undefined');
     }
 
     const license = PublishPackageService.extractManuscriptLicense(manifest, pdfCid);
-    const publishTime = await publishServices.retrieveBlockTimeByManifestCid(node.uuid, manifestCid);
+    const publishTime = demoMode
+      ? Date.now().toString().slice(0, 8)
+      : await publishServices.retrieveBlockTimeByManifestCid(node.uuid, manifestCid);
     const publishDate = PublishPackageService.convertUnixTimestampToDate(publishTime);
     const authors = manifest.authors?.map((author) => author.name);
 
-    const attestations = await attestationService.getAllNodeAttestations(dpid);
+    const attestations = await attestationService.getAllNodeAttestations(node.uuid);
 
     const openCodeAttestation = attestations.find((a) => a.attestationId === 15);
     const openDataAttestation = attestations.find((a) => a.attestationId === 16);
