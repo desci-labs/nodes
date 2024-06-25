bb1e7cdd4ee66253cfa3d0fc74cef98f2e4624a8
kadami <86646883+kadamidev@users.noreply.github.com>
2024-06-25 20:03:43 +0000

    add proxy thumbnail gen route

--- desci-server/src/routes/v1/services.ts
+++ desci-server/src/routes/v1/services.ts
@@ -1,10 +1,17 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import { ephemeralThumbnail } from '../../controllers/proxy/ephemeralThumbnail.js';
 import { orcidDid, orcidProfile } from '../../controllers/proxy/orcidProfile.js';
+import { ensureUser } from '../../internal.js';
 
 const router = Router();
 
+const upload = multer();
+
 router.get('/orcid/profile/:orcidId', [], orcidProfile);
 router.get('/orcid/did/:did', [], orcidDid);
+
+router.post('/thumbnails/ephemeral', [ensureUser, upload.single('file')], ephemeralThumbnail);
 
 export default router;
