237b93d984840f6fa8a71c9acb4341d8655dc8a1
Sina Iman <sina.iman@gmail.com>
2024-07-01 10:54:47 +0200

    parallel dev builds

--- dockerDev.sh
+++ dockerDev.sh
@@ -72,6 +72,7 @@ COMPOSE_HTTP_TIMEOUT=320 docker-compose \
   --file docker-compose.yml \
   --file docker-compose.dev.yml \
   --file docker-compose.repo.yml \
+  --parallel=4 \
   $ADDITIONAL_FLAGS \
   --compatibility \
   up \
