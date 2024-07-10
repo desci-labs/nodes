061a36fca5df674006aa08829019c103754405e9
Sina Iman <sina.iman@gmail.com>
2024-07-10 19:24:59 +0200

    sync commits from main -> dev

--- desci-server/src/routes/v1/services.ts
+++ desci-server/src/routes/v1/services.ts
@@ -3,6 +3,7 @@ import multer from 'multer';
 
 import { ephemeralThumbnail } from '../../controllers/proxy/ephemeralThumbnail.js';
 import { orcidDid, orcidProfile } from '../../controllers/proxy/orcidProfile.js';
+import { logger as parentLogger } from '../../logger.js';
 import { ensureUser } from '../../middleware/permissions.js';
 
 const router = Router();
@@ -12,6 +13,25 @@ const upload = multer();
 router.get('/orcid/profile/:orcidId', [], orcidProfile);
 router.get('/orcid/did/:did', [], orcidDid);
 
-router.post('/thumbnails/ephemeral', [ensureUser, upload.single('file')], ephemeralThumbnail);
+const logger = parentLogger.child({ module: 'Services UploadHandler' });
+
+const wrappedHandler = (req, res, next) => {
+  upload.single('file')(req, res, (err) => {
+    // debugger
+    if (err) {
+      if (err instanceof multer.MulterError) {
+        logger.error({ err, type: 'MulterError' }, 'MulterError');
+        throw err;
+      } else {
+        logger.error({ err }, 'Upload Handler Error encountered');
+        res.status(401).send({ msg: 'unauthorized', code: '5412419' });
+        return;
+      }
+    }
+    next();
+  });
+};
+
+router.post('/thumbnails/ephemeral', [ensureUser, wrappedHandler], ephemeralThumbnail);
 
 export default router;
