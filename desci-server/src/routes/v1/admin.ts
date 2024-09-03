8aa4f2d8ca0570a2d1fb8423b73a76ccda5e4a1d
shadrach <shadrachtemitayo@gmail.com>
2024-09-03 14:31:46 -0500

    list doi records admin api

--- desci-server/src/routes/v1/admin.ts
+++ desci-server/src/routes/v1/admin.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { createCsv, getAnalytics } from '../../controllers/admin/analytics.js';
+import { listDoiRecords } from '../../internal.js';
 import { ensureAdmin } from '../../middleware/ensureAdmin.js';
 import { ensureUser } from '../../middleware/permissions.js';
 
@@ -8,5 +9,6 @@ const router = Router();
 
 router.get('/analytics', [ensureUser, ensureAdmin], getAnalytics);
 router.get('/analytics/csv', [ensureUser, ensureAdmin], createCsv);
+router.get('/doi/list', [ensureUser, ensureAdmin], listDoiRecords);
 
 export default router;
