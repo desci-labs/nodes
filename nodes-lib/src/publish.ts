047630f4e35b84c49245ed8b609e3d92826adf2a
m0ar <edvard@hubinette.me>
2024-10-22 11:17:36 +0200

    nodes-lib: add check for signer/did vs token owner when doing backfill migration

--- nodes-lib/src/publish.ts
+++ nodes-lib/src/publish.ts
@@ -1,28 +1,23 @@
 import { type NodeIDs } from "@desci-labs/desci-codex-lib";
-import { 
+import {
   IndexedNodeVersion,
   getDpidHistory,
   getDraftNode,
   getLegacyHistory,
   prePublishDraftNode,
 } from "./api.js";
-import {
-  dpidPublish,
-  hasDpid,
-} from "./chain.js";
+import { dpidPublish, hasDpid } from "./chain.js";
 import { codexPublish } from "./codex.js";
 import { Signer } from "ethers";
-import { type DID } from "dids";
+import { DID } from "dids";
 import { NoSuchEntryError } from "./errors.js";
+import { fullDidToLcAddress } from "./util/converting.js";
 
 /**
  * Publish node to Codex, potentially migrating history from dPID token.
  * Does *not* automatically register a dPID in the alias registry.
  */
-export const publish = async (
-  uuid: string,
-  didOrSigner: DID | Signer,
-) => {
+export const publish = async (uuid: string, didOrSigner: DID | Signer) => {
   const node = await getDraftNode(uuid);
   const prepubResult = await prePublishDraftNode(uuid);
   const manifestDpid = node.manifestData.dpid?.id;
@@ -32,9 +27,27 @@ export const publish = async (
   const shouldDoMigration = hasDpidInManifest && !hasStreamOnRecord;
 
   let legacyHistory: IndexedNodeVersion[] = [];
+  let legacyOwner: string;
   if (shouldDoMigration) {
-    legacyHistory = await findLegacyHistory(uuid, parseInt(manifestDpid));
-  };
+    const legacyLookup = await findLegacyHistory(uuid, parseInt(manifestDpid));
+    legacyOwner = legacyLookup.owner.toLowerCase();
+    legacyHistory = legacyLookup.versions;
+    const signingAddress =
+      didOrSigner instanceof DID
+        ? fullDidToLcAddress(didOrSigner.parent)
+        : (await didOrSigner.getAddress()).toLowerCase();
+
+    if (legacyOwner !== signingAddress) {
+      console.error(
+        "Refusing to migrate history to stream; signing addresses differs",
+        {
+          legacyOwner,
+          signingAddress,
+        }
+      );
+      throw new Error("Refusing to migrate history; signing addresses differ");
+    }
+  }
 
   // Performs backfill migration if there is no stream on record, otherwise
   // we can send the empty history array and avoid the history query
@@ -53,7 +66,7 @@ export const publish = async (
 
 /**
  * The complete publish flow, including both the dPID registry and Codex.
- * 
+ *
  * @param uuid - Node to publish
  * @param signer - Used to sign TXs for chain, and a SIWE CACAO for ceramic if did argument is not present
  * @param did - An authenticated DID from a DIDSession, better UX as it has a signing capability already
@@ -61,11 +74,11 @@ export const publish = async (
  * @throws (@link WrongOwnerError) if signer address isn't token owner
  * @throws (@link DpidPublishError) if dPID couldnt be registered or updated
  * @deprecated
-*/
+ */
 export const legacyPublish = async (
   uuid: string,
   signer: Signer,
-  did?: DID,
+  did?: DID
 ) => {
   const preexistingDpid = await hasDpid(uuid, signer);
 
@@ -84,12 +97,18 @@ export const legacyPublish = async (
     const publishHistory = preexistingDpid
       ? (await getDpidHistory(uuid)).versions
       : [];
-    ceramicIDs = await codexPublish(chainPubResponse.prepubResult, publishHistory, did ?? signer);
+    ceramicIDs = await codexPublish(
+      chainPubResponse.prepubResult,
+      publishHistory,
+      did ?? signer
+    );
   } catch (e) {
     const err = e as Error;
     console.log("Codex publish failed:", err);
-    console.log(`Publish flow will continue with uuid ${uuid} as dPID registry already has been updated.`);
-  };
+    console.log(
+      `Publish flow will continue with uuid ${uuid} as dPID registry already has been updated.`
+    );
+  }
 
   return {
     ...dpidResult,
@@ -108,18 +127,20 @@ export const legacyPublish = async (
  *
  * This fallback logic can be cleaned up when the old contracts are paused,
  * since the data migration to the alias registry can be made final.
-*/
+ */
 const findLegacyHistory = async (
   uuid: string,
-  dpid: number,
-): Promise<IndexedNodeVersion[]> => {
+  dpid: number
+): Promise<{ owner: string; versions: IndexedNodeVersion[] }> => {
   try {
     return await getLegacyHistory(dpid);
   } catch (e) {
     if (!(e instanceof NoSuchEntryError)) {
       throw e;
-    };
-  };
+    }
+  }
 
-  return (await getDpidHistory(uuid)).versions;
+  const fallbackHistory = await getDpidHistory(uuid);
+
+  return { owner: fallbackHistory.owner, versions: fallbackHistory.versions };
 };
