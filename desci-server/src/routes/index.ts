c3da1588702ad9ab70d4658c66d21575d296686d
shadrach <shadrachtemitayo@gmail.com>
2024-11-05 19:46:20 -0600

    warnings

--- desci-server/src/routes/index.ts
+++ desci-server/src/routes/index.ts
@@ -1,12 +1,9 @@
 import { Router } from 'express';
 
-import { handleCrossrefNotificationCallback } from '../controllers/doi/mint.js';
 import { resolve } from '../controllers/raw/resolve.js';
-import { asyncHandler } from '../utils/asyncHandler.js';
 
 import page404 from './pages/404.js';
 import pageRoot from './pages/root.js';
-import { ensureCrossrefNotifier, identifyEndpoint } from './v1/crossref.js';
 import v1 from './v1/index.js';
 
 const router = Router();
