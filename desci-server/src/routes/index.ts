94cd2f2a896aab0b2c1bfe041b6de837b7295a45
shadrach <shadrachtemitayo@gmail.com>
2024-11-05 19:45:36 -0600

    clean up

--- desci-server/src/routes/index.ts
+++ desci-server/src/routes/index.ts
@@ -13,18 +13,6 @@ const router = Router();
 
 router.use(`/v1`, v1);
 
-// potential notification fallback catch
-router.post(
-  '/crossref/callback',
-  [identifyEndpoint('/crossref/callback'), ensureCrossrefNotifier],
-  asyncHandler(handleCrossrefNotificationCallback),
-);
-router.post(
-  '/crossref/callback/v1/crossref/callback',
-  [identifyEndpoint('/crossref/callback/v1/crossref/callback'), ensureCrossrefNotifier],
-  asyncHandler(handleCrossrefNotificationCallback),
-);
-
 router.get('/:query*', resolve);
 
 router.use(pageRoot);
