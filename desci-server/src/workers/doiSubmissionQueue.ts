4cbeac2ae2730482b44c52339e6667e8fda09e83
shadrach-tayo <shadrachtemitayo@gmail.com>
2024-11-12 06:50:09 -0600

    distributed locking for doi submission job queue

--- desci-server/src/workers/doiSubmissionQueue.ts
+++ desci-server/src/workers/doiSubmissionQueue.ts
@@ -2,6 +2,7 @@ import { CronJob } from 'cron';
 
 import { prisma } from '../client.js';
 import { logger as parentLogger } from '../logger.js';
+import { lockService } from '../redisClient.js';
 import { EmailTypes, sendEmail } from '../services/email.js';
 import { doiService } from '../services/index.js';
 import { DiscordChannel, discordNotify, DiscordNotifyType } from '../utils/discordUtils.js';
@@ -28,44 +29,54 @@ export const onTick = async () => {
   if (pendingSubmissions.length === 0) return;
   logger.info({ pendingSubmissions }, 'pending submission');
   const processed = await asyncMap(pendingSubmissions, async (job) => {
-    const isResolved = await pingDoi(job.uniqueDoi);
-    if (isResolved) {
-      await doiService.onRegistrationSuccessful(job);
-      discordNotify({
-        channel: DiscordChannel.DoiMinting,
-        title: 'DOI Registration ✅',
-        type: DiscordNotifyType.SUCCESS,
-        message: `DOI: https://doi.org/${job.uniqueDoi}
-        DPID: ${job.dpid}`,
-      });
-
-      const node = await prisma.node.findFirst({
-        where: { uuid: job.uuid },
-        include: { owner: { select: { email: true, name: true } } },
-      });
-
-      if (node.owner.email) {
-        sendEmail({
-          type: EmailTypes.DoiMinted,
-          payload: {
-            name: node.owner.name,
-            doi: job.uniqueDoi,
-            dpid: job.dpid,
-            to: node.owner.email,
-            title: node.title,
-          },
+    const taskLock = await lockService.aquireLock(job.batchId);
+    logger.info({ job: job.batchId, taskLock }, 'ACQUIRE Lock');
+    if (!taskLock) return undefined;
+    try {
+      const isResolved = await pingDoi(job.uniqueDoi);
+      if (isResolved) {
+        await doiService.onRegistrationSuccessful(job);
+        discordNotify({
+          channel: DiscordChannel.DoiMinting,
+          title: 'DOI Registration ✅',
+          type: DiscordNotifyType.SUCCESS,
+          message: `DOI: https://doi.org/${job.uniqueDoi}
+          DPID: ${job.dpid}`,
         });
+
+        const node = await prisma.node.findFirst({
+          where: { uuid: job.uuid },
+          include: { owner: { select: { email: true, name: true } } },
+        });
+
+        if (node.owner.email) {
+          sendEmail({
+            type: EmailTypes.DoiMinted,
+            payload: {
+              name: node.owner.name,
+              doi: job.uniqueDoi,
+              dpid: job.dpid,
+              to: node.owner.email,
+              title: node.title,
+            },
+          });
+        }
       }
+      return { doi: job.uniqueDoi, jobId: job.id, isResolved };
+    } catch (err) {
+      logger.warn({ err }, 'pendingSubmissions cron error');
+      return { doi: job.uniqueDoi, jobId: job.id, isResolved: false };
+    } finally {
+      // await lockService.freeLock(job.batchId);
     }
-    return { doi: job.uniqueDoi, jobId: job.id, isResolved };
   });
   logger.trace({ processed }, 'Exiting Job with results');
 };
 
 export const SubmissionQueueJob = new CronJob(
   // schedule cron to run every hour
-  // '*/10 * * * * *', // 10 seconds (for local test)
-  '0 * * * * *', // 1 hour
+  // '* * * * *', // 10 seconds (for local test)
+  '0 * * * *', // 1 hour
   onTick, // onTick
   null, // onComplete
   false, // start
