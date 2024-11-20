55828d71cfabd2577e0020b04cb41cf5d6e733c2
kadami <86646883+kadamidev@users.noreply.github.com>
2024-11-20 17:43:40 +0000

    fix existing userId check

--- desci-server/src/controllers/nodes/contributions/create.ts
+++ desci-server/src/controllers/nodes/contributions/create.ts
@@ -57,7 +57,7 @@ export const addContributor = async (req: AddContributorRequest, res: Response<A
   if (!userId && !email && !orcid) {
     return res.status(400).json({ error: 'userId, Email or Orcid required' });
   }
-  // debugger;
+  // debugger; //
   // Add contributor to the db
   try {
     const contributorAdded = await contributorService.addNodeContribution({
@@ -70,7 +70,7 @@ export const addContributor = async (req: AddContributorRequest, res: Response<A
     });
     if (!contributorAdded) throw Error('Failed to add contributor');
 
-    if (!email && contributorAdded.userId !== undefined) {
+    if (!email && !!contributorAdded.userId) {
       // If the contributor being added has an existing account, their email is available on their profile.
       const invitedContributor = await prisma.user.findUnique({ where: { id: contributorAdded.userId } });
       if (invitedContributor?.email) email = invitedContributor.email;
