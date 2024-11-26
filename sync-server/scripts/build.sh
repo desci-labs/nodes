8be3f24b6d1150a2ae3a65e5cd0f0c2ea0fda62b
shadrach <shadrachtemitayo@gmail.com>
2024-11-26 13:58:41 -0600

    fix: sed gnu error

--- sync-server/scripts/build.sh
+++ sync-server/scripts/build.sh
@@ -10,11 +10,11 @@ yarn install
 if [[ "${ENVIRONMENT}" = 'test' ]]; then
     awk '{gsub("<DATABASE_URL>", "postgresql://walter:white@host.docker.internal:5434/boilerplate", $0); print}' template.toml >wrangler.toml
     yarn compile
-    sed -i '' "s/<DATABASE_URL>/postgresql:\/\/walter:white@host.docker.internal:5434\/boilerplate/g" '.wrangler/dist/index.js'
+    sed -i "" -e "s/<DATABASE_URL>/postgresql:\/\/walter:white@host.docker.internal:5434\/boilerplate/g" '.wrangler/dist/index.js'
     yarn build
 else
     awk '{gsub("<DATABASE_URL>", "postgresql://walter:white@host.docker.internal:5433/boilerplate", $0); print}' template.toml >wrangler.toml
     yarn compile
-    sed -i '' "s/<DATABASE_URL>/postgresql:\/\/walter:white@host.docker.internal:5433\/boilerplate/g" '.wrangler/dist/index.js'
+    sed -i "" -e "s/<DATABASE_URL>/postgresql:\/\/walter:white@host.docker.internal:5433\/boilerplate/g" '.wrangler/dist/index.js'
     yarn build
 fi
