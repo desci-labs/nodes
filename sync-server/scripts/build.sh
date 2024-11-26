f8efbf0590bde29cbca8c43d85c3dd688378548e
shadrach <shadrachtemitayo@gmail.com>
2024-11-26 15:48:52 -0600

    remove worker build in yarn test

--- sync-server/scripts/build.sh
+++ sync-server/scripts/build.sh
@@ -8,12 +8,14 @@ echo "Building sync server for $ENVIRONMENT"
 # yarn cache clean --all
 yarn install
 if [[ "${ENVIRONMENT}" = 'test' ]]; then
+    echo "Running test env build: $ENVIRONMENT"
     awk '{gsub("<DATABASE_URL>", "postgresql://walter:white@host.docker.internal:5434/boilerplate", $0); print}' template.toml >wrangler.toml
     yarn compile
     mv .wrangler/dist/index.js .wrangler/dist/index.js.old
     sed "s/<DATABASE_URL>/postgresql:\/\/walter:white@host.docker.internal:5434\/boilerplate/g" .wrangler/dist/index.js.old >'.wrangler/dist/index.js'
     yarn build
 else
+    echo "Running dev env build: $ENVIRONMENT"
     awk '{gsub("<DATABASE_URL>", "postgresql://walter:white@host.docker.internal:5433/boilerplate", $0); print}' template.toml >wrangler.toml
     yarn compile
     mv .wrangler/dist/index.js .wrangler/dist/index.js.old
