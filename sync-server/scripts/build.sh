b4d6438319ea0e900272c15f7b62a0af8dd10744
shadrach <shadrachtemitayo@gmail.com>
2024-11-26 13:47:56 -0600

    add install step in ci workflow

--- sync-server/scripts/build.sh
+++ sync-server/scripts/build.sh
@@ -5,8 +5,8 @@ set -e
 ENVIRONMENT=$1
 echo "Building sync server for $ENVIRONMENT"
 # REPLACE <DATABASE_URL> pattern in wrangler.toml with value
-yarn cache clean --all
-yarn
+# yarn cache clean --all
+yarn install
 if [[ "${ENVIRONMENT}" = 'test' ]]; then
     awk '{gsub("<DATABASE_URL>", "postgresql://walter:white@host.docker.internal:5434/boilerplate", $0); print}' template.toml >wrangler.toml
     yarn compile
