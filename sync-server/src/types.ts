148a05143fa8e9e65b517c5b0414a67dea26dc3f
shadrach <shadrachtemitayo@gmail.com>
2024-11-26 07:11:08 -0600

    add sync-server to workflow path

--- sync-server/src/types.ts
+++ sync-server/src/types.ts
@@ -1,8 +1,6 @@
 import { Hyperdrive } from '@cloudflare/workers-types';
 
 export interface Env {
-  // If you set another name in wrangler.toml as the value for 'binding',
-  // replace "HYPERDRIVE" with the variable name you defined.
   NODES_DB: Hyperdrive;
   DATABASE_URL: string;
   DB_TABLE: string;
