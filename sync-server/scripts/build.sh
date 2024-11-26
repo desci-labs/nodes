4f71369b11b43d72b9a41f052edd9506495983e7
shadrach <shadrachtemitayo@gmail.com>
2024-11-26 15:32:05 -0600

    sed ?

--- sync-server/scripts/build.sh
+++ sync-server/scripts/build.sh
@@ -10,11 +10,13 @@ yarn install
 if [[ "${ENVIRONMENT}" = 'test' ]]; then
     awk '{gsub("<DATABASE_URL>", "postgresql://walter:white@host.docker.internal:5434/boilerplate", $0); print}' template.toml >wrangler.toml
     yarn compile
-    sed -i "" -e "s/<DATABASE_URL>/postgresql:\/\/walter:white@host.docker.internal:5434\/boilerplate/g" '.wrangler/dist/index.js'
+    mv .wrangler/dist/index.js .wrangler/dist/index.js.old
+    sed "s/<DATABASE_URL>/postgresql:\/\/walter:white@host.docker.internal:5434\/boilerplate/g" .wrangler/dist/index.js.old >'.wrangler/dist/index.js'
     yarn build
 else
     awk '{gsub("<DATABASE_URL>", "postgresql://walter:white@host.docker.internal:5433/boilerplate", $0); print}' template.toml >wrangler.toml
     yarn compile
-    sed -i "" -e "s/<DATABASE_URL>/postgresql:\/\/walter:white@host.docker.internal:5433\/boilerplate/g" '.wrangler/dist/index.js'
+    mv .wrangler/dist/index.js .wrangler/dist/index.js.old
+    sed "s/<DATABASE_URL>/postgresql:\/\/walter:white@host.docker.internal:5433\/boilerplate/g" .wrangler/dist/index.js.old >.wrangler/dist/index.js
     yarn build
 fi
