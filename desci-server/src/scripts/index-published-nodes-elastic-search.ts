e06e29c3fe864bd3af159c2e5cb7096ab1274486
kadami <86646883+kadamidev@users.noreply.github.com>
2025-02-03 23:17:38 +1100

    add offsetting to backfill script

--- desci-server/src/scripts/index-published-nodes-elastic-search.ts
+++ desci-server/src/scripts/index-published-nodes-elastic-search.ts
@@ -10,6 +10,7 @@ const logger = parentLogger.child({ module: 'SCRIPTS::IndexPublishedNodesES' });
 
 async function main() {
   logger.info('[ES Native Node Backfill] Script starting');
+  const { start, end } = getScriptEnvs();
   const publishedNodes = await prisma.nodeVersion.findMany({
     where: {
       OR: [{ transactionId: { not: null } }, { commitId: { not: null } }],
@@ -33,10 +34,21 @@ async function main() {
     },
   });
 
-  logger.info(`[ES Native Node Backfill] Found ${nodeUuids.length} published nodes`);
+  const slicedNodeUuids = nodeUuids.slice(start, end ? end + 1 : undefined);
+
+  logger.info(
+    { start, end },
+    `[ES Native Node Backfill] Found ${nodeUuids.length} published nodes, selected ${slicedNodeUuids.length} published nodes.`,
+  );
+  if (start || end) {
+    logger.info(
+      { start, end, firstNodeId: slicedNodeUuids[0].id, lastNodeId: slicedNodeUuids[slicedNodeUuids.length - 1].id },
+      `[ES Native Node Backfill] START and END are set, will only index nodes between ${start} and ${end}`,
+    );
+  }
 
   let i = 0;
-  for (const { id, uuid } of nodeUuids) {
+  for (const { id, uuid } of slicedNodeUuids) {
     logger.info({ uuid, nodeId: id }, `[ES Native Node Backfill] Indexing node ${i}/${nodeUuids.length}`);
     await ElasticNodesService.indexResearchObject(uuid);
     logger.info({ uuid, nodeId: id }, `[ES Native Node Backfill] Completed indexing node ${i}/${nodeUuids.length}`);
@@ -45,6 +57,25 @@ async function main() {
 
   logger.info('[ES Native Node Backfill] Script finished executing');
   process.exit(0);
+}
+
+function getScriptEnvs() {
+  const start = process.env.START ? Number(process.env.START) : undefined;
+  const end = process.env.END ? Number(process.env.END) : undefined;
+
+  if (start !== undefined && isNaN(start)) {
+    throw new Error('START must be a valid number');
+  }
+
+  if (end !== undefined && isNaN(end)) {
+    throw new Error('END must be a valid number');
+  }
+
+  if (start !== undefined && end !== undefined && start > end) {
+    throw new Error('START cannot be greater than END');
+  }
+
+  return { start, end };
 }
 
 main();
