4c3dbfdf7c1b4e46b9c842575939b2fd40eeb630
shadrach <shadrachtemitayo@gmail.com>
2025-03-24 09:28:37 +0100

    feat: api to cancel user subscriptions, fix: undefined bug in createSubmission

--- desci-server/src/routes/v1/communities/submissions.ts
+++ desci-server/src/routes/v1/communities/submissions.ts
@@ -4,8 +4,8 @@ import {
   createSubmission,
   updateSubmissionStatus,
   getSubmission,
+  cancelUserSubmission,
 } from '../../../controllers/communities/submissions.js';
-import { ensureNodeAccess } from '../../../middleware/authorisation.js';
 import { ensureUser } from '../../../middleware/permissions.js';
 import { validate } from '../../../middleware/validator.js';
 import { asyncHandler } from '../../../utils/asyncHandler.js';
@@ -23,5 +23,7 @@ router.patch(
 );
 
 router.get('/:submissionId', [ensureUser, validate(getSubmissionSchema)], asyncHandler(getSubmission));
+
+router.delete('/:submissionId', [ensureUser], asyncHandler(cancelUserSubmission));
 
 export default router;
