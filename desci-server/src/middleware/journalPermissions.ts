dfadcc7f07a4a9a939f64010c6583b23dbe1dc03
kadami <86646883+kadamidev@users.noreply.github.com>
2025-05-14 21:57:28 +1000

    add journal permissions matrix

--- desci-server/src/middleware/journalPermissions.ts
+++ desci-server/src/middleware/journalPermissions.ts
@@ -11,6 +11,49 @@ interface JournalParams {
   journalId: string;
 }
 
+export enum JournalRole {
+  CHIEF_EDITOR = 'CHIEF_EDITOR',
+  ASSOCIATE_EDITOR = 'ASSOCIATE_EDITOR',
+  REFEREE = 'REFEREE',
+  USER = 'USER',
+}
+
+export enum JournalAction {
+  INVITE_EDITOR = 'INVITE_EDITOR',
+  INVITE_REFEREE = 'INVITE_REFEREE',
+  SUBMIT_REVIEW = 'SUBMIT_REVIEW',
+  VIEW_STATUS = 'VIEW_STATUS',
+}
+
+export type JournalPermissionMatrix = {
+  [R in JournalRole]: Partial<Record<JournalAction, boolean>>;
+};
+
+const JournalPermissionMatrix: JournalPermissionMatrix = {
+  [JournalRole.CHIEF_EDITOR]: {
+    [JournalAction.INVITE_EDITOR]: true,
+    [JournalAction.INVITE_REFEREE]: true,
+    [JournalAction.SUBMIT_REVIEW]: true,
+    [JournalAction.VIEW_STATUS]: true,
+  },
+  [JournalRole.ASSOCIATE_EDITOR]: {
+    [JournalAction.INVITE_REFEREE]: true,
+    [JournalAction.SUBMIT_REVIEW]: true,
+    [JournalAction.VIEW_STATUS]: true,
+  },
+  [JournalRole.REFEREE]: {
+    [JournalAction.SUBMIT_REVIEW]: true,
+    [JournalAction.VIEW_STATUS]: true,
+  },
+  [JournalRole.USER]: {
+    [JournalAction.VIEW_STATUS]: true,
+  },
+};
+
+export function can(role: JournalRole, action: JournalAction): boolean {
+  return JournalPermissionMatrix[role]?.[action] ?? false;
+}
+
 export const ensureJournalRole = (requiredRole: EditorRole) => {
   return async (req: AuthenticatedRequest<JournalParams>, res: Response, next: NextFunction) => {
     try {
