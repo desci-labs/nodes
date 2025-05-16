69e592fb025d03307c90f510d7e25bc0aeb571db
kadami <86646883+kadamidev@users.noreply.github.com>
2025-05-17 07:09:38 +1000

    route updates

--- desci-server/src/routes/v1/journals/index.ts
+++ desci-server/src/routes/v1/journals/index.ts
@@ -3,13 +3,21 @@ import { Router } from 'express';
 
 import { editorInviteDecision } from '../../../controllers/journals/invites/editorInviteDecision.js';
 import { inviteEditor } from '../../../controllers/journals/invites/inviteEditor.js';
+import { listJournalsController } from '../../../controllers/journals/list.js';
 import { createJournalController } from '../../../controllers/journals/management/create.js';
+import { removeEditorController } from '../../../controllers/journals/management/removeEditor.js';
 import { updateJournalController } from '../../../controllers/journals/management/update.js';
+import { updateEditorRoleController } from '../../../controllers/journals/management/updateRole.js';
+import { showJournalController } from '../../../controllers/journals/show.js';
 import { attachUser } from '../../../middleware/attachUser.js';
 import { ensureJournalRole } from '../../../middleware/journalPermissions.js';
 import { ensureUser } from '../../../middleware/permissions.js';
 
 const router = Router();
+
+// General
+router.get('/journals', listJournalsController);
+router.get('/journals/:journalId', showJournalController);
 
 // Invites
 router.post(
@@ -22,5 +30,15 @@ router.post('/journals/:journalId/invitation/editor', [attachUser], editorInvite
 // Management
 router.post('/journals', [ensureUser], createJournalController);
 router.put('/journals/:journalId', [ensureUser, ensureJournalRole(EditorRole.CHIEF_EDITOR)], updateJournalController);
+router.patch(
+  '/journals/:journalId/editors/:editorId',
+  [ensureUser, ensureJournalRole(EditorRole.CHIEF_EDITOR)],
+  updateEditorRoleController,
+);
+router.delete(
+  '/journals/:journalId/editors/:editorId',
+  [ensureUser, ensureJournalRole(EditorRole.CHIEF_EDITOR)],
+  removeEditorController,
+);
 
 export default router;
