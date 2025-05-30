0e055f06b899d57112969ce7ee94745eaf09eb05
kadami <86646883+kadamidev@users.noreply.github.com>
2025-05-30 18:08:38 +1000

    add origin logging

--- reverse-proxy/src/index.ts
+++ reverse-proxy/src/index.ts
@@ -1,9 +1,17 @@
-import express from 'express';
-import { ServerResponse } from 'http';
-import { createProxyMiddleware, responseInterceptor } from 'http-proxy-middleware';
-import { buildMappingFromEnv, getSensitiveStrings, redactSensitive } from './util.js';
+import express from "express";
+import { ServerResponse } from "http";
+import {
+  createProxyMiddleware,
+  responseInterceptor,
+} from "http-proxy-middleware";
+import {
+  buildMappingFromEnv,
+  getLogger,
+  getSensitiveStrings,
+  redactSensitive,
+} from "./util.js";
 
-const SENSIBLE_KEY_REGEX = '^\/[a-zA-Z0-9_-]+';
+const SENSIBLE_KEY_REGEX = "^/[a-zA-Z0-9_-]+";
 const rSensibleKey = RegExp(SENSIBLE_KEY_REGEX);
 
 const mapping = buildMappingFromEnv();
@@ -15,67 +23,67 @@ console.log(
 
 const sensitiveStrings = getSensitiveStrings(mapping);
 
-/** 
+/**
  * Checks that a single mapping has a sensible key and that the
  * target is a valid URL.
-*/
-const assertValidMapping = (
-  [key, target]: [string, string]
-): void => {
+ */
+const assertValidMapping = ([key, target]: [string, string]): void => {
   if (!rSensibleKey.test(key)) {
     throw new Error(`Invalid mapping key: ${key}`);
-  };
-  
+  }
+
   try {
     new URL(target);
   } catch {
     throw new Error(`Invalid URL in target: ${target}`);
-  };
+  }
 };
 
-console.log("Asserting valid keys and targets...")
+console.log("Asserting valid keys and targets...");
 Object.entries(mapping).forEach(assertValidMapping);
-console.log("All good. Starting proxy...")
+console.log("All good. Starting proxy...");
 
-const createProxy = (target: string) => createProxyMiddleware({
-  target,
-  // Enables successful TLS handshake
-  changeOrigin: true,
-  // Cut the key so it doesn't get applied to target URL
-  pathRewrite: { [SENSIBLE_KEY_REGEX]: "" },
-  // Log each proxy call to console
-  logger: console,
-  // Disable automatically sending response, handled by responseInterceptor
-  selfHandleResponse: true,
-  on: {
-    error: (_err, _req, res) => (res as ServerResponse)
-      .writeHead(500, { 'Content-Type': 'text/plain' })
-      .end('Something went wrong while proxying the request'),
-    proxyRes: responseInterceptor(
-      async (responseBuffer, proxyRes, _req, res) => {
-        res.statusCode = proxyRes.statusCode ?? 200;
-        return redactSensitive(
-          sensitiveStrings,
-          responseBuffer.toString("utf8")
-        );
-      }),
+const createProxy = (target: string) =>
+  createProxyMiddleware({
+    target,
+    // Enables successful TLS handshake
+    changeOrigin: true,
+    // Cut the key so it doesn't get applied to target URL
+    pathRewrite: { [SENSIBLE_KEY_REGEX]: "" },
+    // Log each proxy call to console
+    ...getLogger(),
+    // Disable automatically sending response, handled by responseInterceptor
+    selfHandleResponse: true,
+    on: {
+      error: (_err, _req, res) =>
+        (res as ServerResponse)
+          .writeHead(500, { "Content-Type": "text/plain" })
+          .end("Something went wrong while proxying the request"),
+      proxyRes: responseInterceptor(
+        async (responseBuffer, proxyRes, _req, res) => {
+          res.statusCode = proxyRes.statusCode ?? 200;
+          return redactSensitive(
+            sensitiveStrings,
+            responseBuffer.toString("utf8")
+          );
+        }
+      ),
     },
-});
+  });
 
 const app = express();
 
-Object.entries(mapping)
-  .forEach(([alias, target]) => app.use(alias, createProxy(target)));
+Object.entries(mapping).forEach(([alias, target]) =>
+  app.use(alias, createProxy(target))
+);
 
 app.use("/healthcheck", (_req, res) => {
   res.send("I'm doing OK");
 });
 
-app.use(
-  (req, res) => {
-    console.log(`Got request for unmapped path ${req.url}, responding 404.`);
-    res.writeHead(404, `No route found for ${req.url}`).end();
-  },
-);
+app.use((req, res) => {
+  console.log(`Got request for unmapped path ${req.url}, responding 404.`);
+  res.writeHead(404, `No route found for ${req.url}`).end();
+});
 
 app.listen(5678);
--- reverse-proxy/src/util.ts
+++ reverse-proxy/src/util.ts
@@ -2,28 +2,29 @@ export type Mapping = Record<string, string>;
 
 /**
  * Build a route mapping from envvars prefixed `PROXY_MAPPING_`
-*/
-export const buildMappingFromEnv = (): Mapping => Object.fromEntries(
-  Object.entries(process.env as { [s:string]: string } )
-  .filter(([k, _]) => k.startsWith("PROXY_MAPPING_"))
-  .map(([k, v]) => [k.replace("PROXY_MAPPING_", ""), v])
-  .map(([k, v]) => ["/" + k.toLowerCase(), v])
-);
+ */
+export const buildMappingFromEnv = (): Mapping =>
+  Object.fromEntries(
+    Object.entries(process.env as { [s: string]: string })
+      .filter(([k, _]) => k.startsWith("PROXY_MAPPING_"))
+      .map(([k, v]) => [k.replace("PROXY_MAPPING_", ""), v])
+      .map(([k, v]) => ["/" + k.toLowerCase(), v])
+  );
 
 /**
  * Find sensitive strings in mapped URL's
-*/
+ */
 export const getSensitiveStrings = (mapping: Mapping) => {
   const urls = Object.values(mapping);
 
   const alchemyTokens = urls
-    .filter(url => url.includes("alchemy.com"))
+    .filter((url) => url.includes("alchemy.com"))
     /* 32-char alphanum token with dashes and underscores */
-    .map(url => url.match(/[a-zA-Z0-9_-]{32}/))
+    .map((url) => url.match(/[a-zA-Z0-9_-]{32}/))
     /* Remove potential null match for tokenless url */
-    .flatMap(maybeMatch => maybeMatch ? [maybeMatch] : [])
+    .flatMap((maybeMatch) => (maybeMatch ? [maybeMatch] : []))
     /* Get the match from RexExpMatchArray */
-    .map(matchArr => matchArr[0]);
+    .map((matchArr) => matchArr[0]);
 
   return alchemyTokens;
 };
@@ -34,5 +35,31 @@ export const redactSensitive = (
 ): string =>
   sensitiveStrings.reduce(
     (acc, nextSecret) => acc.replaceAll(nextSecret, "[redacted]"),
-    dirty, // initial accumulator
+    dirty // initial accumulator
   );
+
+/**
+ * Helper to get a logger that logs the origin of the request if the process.env.PROXY_ORIGIN_LOGGING is set to true
+ */
+export const getLogger = () => {
+  const originLoggingEnabled = process.env.PROXY_ORIGIN_LOGGING === "true";
+
+  const logger = (provider: any) => {
+    const createLogMethod = (consoleMethod: (...args: any[]) => void) => {
+      return (message: string, ...args: any[]) => {
+        const req = provider.lastReq;
+        const ip = req ? req.ip || req.socket?.remoteAddress : "unknown_ip";
+        consoleMethod(`[${ip}] ${message}`, ...args);
+      };
+    };
+
+    return {
+      log: createLogMethod(console.log),
+      info: createLogMethod(console.info),
+      warn: createLogMethod(console.warn),
+      error: createLogMethod(console.error),
+    };
+  };
+
+  return originLoggingEnabled ? { logProvider: logger } : { logger: console };
+};
