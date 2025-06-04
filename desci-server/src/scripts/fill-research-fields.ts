471329d411dc26fce8dc5dc03226d2e2163665a3
shadrach <shadrachtemitayo@gmail.com>
2025-06-04 11:44:48 +0100

    refactor json import

--- desci-server/src/scripts/fill-research-fields.ts
+++ desci-server/src/scripts/fill-research-fields.ts
@@ -1,8 +1,8 @@
 import { prisma } from '../client.js';
+import researchFieldsData from '../data/fields.json' assert { type: 'json' };
 
 const main = async () => {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const researchFieldsData = require('../data/fields.json');
   const parsedFields: { name: string }[] = researchFieldsData.map((name) => ({ name }));
 
   const fields = await prisma.researchFields.createMany({ data: parsedFields, skipDuplicates: true });
