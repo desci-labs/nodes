d6bea984fb1fdcf78ad82a2ef49a9242c6183289
kadami <86646883+kadamidev@users.noreply.github.com>
2025-06-18 07:35:34 +1000

    remove invalid test

--- desci-server/test/integration/journals/journalSubmission.test.ts
+++ desci-server/test/integration/journals/journalSubmission.test.ts
@@ -654,16 +654,6 @@ describe('Journal Submission Service', () => {
       expect(response.body.ok).to.be.true;
     });
 
-    it('should prevent CHIEF EDITOR from accepting submission', async () => {
-      response = await request
-        .post(`/v1/journals/${journal.id}/submissions/${submission.id}/accept`)
-        .set('authorization', `Bearer ${chiefEditor.token}`)
-        .send();
-
-      expect(response.status).to.equal(403);
-      expect(response.body.message).to.equal('Forbidden - Insufficient permissions');
-    });
-
     it('should prevent Referee from accepting submission', async () => {
       response = await request
         .post(`/v1/journals/${journal.id}/submissions/${submission.id}/accept`)
