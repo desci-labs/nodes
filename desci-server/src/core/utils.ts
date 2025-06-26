0342efaf3afec4117becf533d51856d5af0dbd2b
shadrach <shadrachtemitayo@gmail.com>
2025-06-26 17:55:31 +0100

    fix: api response object formatter breaks Date

--- desci-server/src/core/utils.ts
+++ desci-server/src/core/utils.ts
@@ -2,7 +2,8 @@ export function sanitizeBigInts(obj: any): any {
   if (Array.isArray(obj)) {
     return obj.map(sanitizeBigInts);
   } else if (obj && typeof obj === 'object') {
-    return Object.fromEntries(Object.entries(obj).map(([k, v]) => [k, sanitizeBigInts(v)]));
+    const keys = Object.keys(obj);
+    return keys.length > 0 ? Object.fromEntries(Object.entries(obj).map(([k, v]) => [k, sanitizeBigInts(v)])) : obj;
   } else if (typeof obj === 'bigint') {
     return obj.toString();
   } else {
