3be90bb314ce122642906295e4b9660644fcfb7b
kadami <86646883+kadamidev@users.noreply.github.com>
2025-12-12 04:39:50 +1100

    add public ipfs resolver fallback to isolated media server

--- desci-media-isolated/src/config/index.ts
+++ desci-media-isolated/src/config/index.ts
@@ -1,4 +1,5 @@
 export const IPFS_GATEWAY = process.env.IPFS_GATEWAY;
+export const IPFS_PUBLIC_GATEWAY = process.env.IPFS_PUBLIC_GATEWAY || 'https://pub.desci.com/ipfs';
 export const TEMP_DIR = '.temp';
 export const THUMBNAIL_FILES_DIR = `/files`;
 export const PDF_FILES_DIR = `/files/pdf`;
